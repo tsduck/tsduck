b451c58d4a6a2f013ddc03b84b6e30e67de4cb4a
Thierry Lelegard <thierry@lelegard.fr>
2017-08-24 10:54:58 +0200

    Added sample script to list services

---
+++ sample/list-services.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# List all services from a set of TS files.
+
+group_digits() { sed <<<$1 -r ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L'; }
+
+echo "Service name                             Provider        Access     TS id Sv id      Bitrate"
+echo "---------------------------------------- --------------- ---------- ----- ----- ------------"
+
+for tsfile in "$@"; do
+    tsanalyze "$tsfile" --normalized | \
+        grep -a '^service:' | \
+        sed -e 's/\r//g' | \
+        while read line; do
+            id=${line/*:id=/}
+            tsid=${line/*:tsid=/}
+            access=${line/*:access=/}
+            bitrate=${line/*:bitrate=/}
+            provider=${line/*:provider=/}
+            printf '%-40.40s %-15.15s %-10.10s %5s %5s %12s\n' \
+                   "${line/*:name=/}" \
+                   "${provider/:*/}" \
+                   "${access/:*/}" \
+                   "${tsid/:*/}" \
+                   "${id/:*/}" \
+                   $(group_digits "${bitrate/:*/}")
+        done
+done | sort -d -i
