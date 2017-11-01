bc89cd9b3eeb64deb30ff72af1e2383e67281413
Thierry Lelegard <thierry@lelegard.fr>
2017-11-01 23:51:33 +0100

    Moved Hexa functions to UString

--- src/libtsduck/tsUString.cpp
+++ src/libtsduck/tsUString.cpp
@@ -761,7 +761,7 @@ bool ts::UString::getLine(std::istream& strm)
     }
     else {
         // Remove potential trailing mixed CR/LF characters
-        size_t len;
+        size_type len;
         for (len = line.size(); len > 0 && (line[len - 1] == '\r' || line[len - 1] == '\n'); --len) {
         }
         line.resize(len);
@@ -813,10 +813,245 @@ bool ts::UString::hexaDecodeAppend(ts::ByteBlock& result)
 
 
 //----------------------------------------------------------------------------
+// Build a multi-line string containing the hexadecimal dump of a memory area.
+// Wrapper methods.
+//----------------------------------------------------------------------------
+
+ts::UString ts::UString::Dump(const void *data,
+                              size_type size,
+                              uint32_t flags,
+                              size_type indent,
+                              size_type line_width,
+                              size_type init_offset,
+                              size_type inner_indent)
+{
+    UString s;
+    s.appendDump(data, size, flags, indent, line_width, init_offset, inner_indent);
+    return s;
+}
+
+ts::UString ts::UString::Dump(const ByteBlock& bb,
+                              uint32_t flags,
+                              size_type indent,
+                              size_type line_width,
+                              size_type init_offset,
+                              size_type inner_indent)
+{
+    UString s;
+    s.appendDump(bb.data(), bb.size(), flags, indent, line_width, init_offset, inner_indent);
+    return s;
+}
+
+void ts::UString::appendDump(const ByteBlock& bb,
+                             uint32_t flags,
+                             size_type indent,
+                             size_type line_width,
+                             size_type init_offset,
+                             size_type inner_indent)
+{
+    appendDump(bb.data(), bb.size(), flags, indent, line_width, init_offset, inner_indent);
+}
+
+
+//----------------------------------------------------------------------------
+// Build a multi-line string containing the hexadecimal dump of a memory area.
+//----------------------------------------------------------------------------
+
+void ts::UString::appendDump(const void *data,
+                             size_type size,
+                             uint32_t flags,
+                             size_type indent,
+                             size_type line_width,
+                             size_type init_offset,
+                             size_type inner_indent)
+{
+    const uint8_t* raw = static_cast<const uint8_t*>(data);
+
+    // Make sure we have something to display (default is hexa)
+    if ((flags & (HEXA | C_STYLE | BINARY | BIN_NIBBLE | ASCII)) == 0) {
+        flags |= HEXA;
+    }
+    if ((flags & COMPACT) != 0) {
+        // COMPACT implies SINGLE_LINE.
+        flags |= SINGLE_LINE;
+    }
+
+    // Width of an hexa byte: "XX" (2) or "0xXX," (5)
+    size_type hexa_width = 0;
+    UString byte_prefix;
+    UString byte_suffix;
+
+    if (flags & C_STYLE) {
+        hexa_width  = 5;
+        byte_prefix = u"0x";
+        byte_suffix = u",";
+        flags |= HEXA; // Enforce hexa flag
+    }
+    else if (flags & (HEXA | SINGLE_LINE)) {
+        hexa_width  = 2;
+    }
+    else {
+        hexa_width  = 0;
+    }
+
+    // Specific case: simple dump, everything on one line.
+    if (flags & SINGLE_LINE) {
+        reserve(length() + (hexa_width + 1) * size);
+        for (size_type i = 0; i < size; ++i) {
+            if (i > 0 && (flags & COMPACT) == 0) {
+                append(1, u' ');
+            }
+            append(byte_prefix);
+            append(Hexa(raw[i], 0, UString(), false, true));
+            append(byte_suffix);
+        }
+        return;
+    }
+
+    // Width of offset field
+    size_type offset_width;
+
+    if ((flags & OFFSET) == 0) {
+        offset_width = 0;
+    }
+    else if (flags & WIDE_OFFSET) {
+        offset_width = 8;
+    }
+    else if (init_offset + size <= 0x10000) {
+        offset_width = 4;
+    }
+    else {
+        offset_width = 8;
+    }
+
+    // Width of a binary byte
+    size_type bin_width;
+
+    if (flags & BIN_NIBBLE) {
+        bin_width = 9;
+        flags |= BINARY;  // Enforce binary flag
+    }
+    else if (flags & BINARY) {
+        bin_width = 8;
+    }
+    else {
+        bin_width = 0;
+    }
+
+    // Pre-allocation to avoid too frequent reallocations.
+    reserve(length() + indent + inner_indent + (hexa_width + bin_width + 5) * size);
+
+    // Number of non-byte characters
+    size_type add_width = indent + inner_indent;
+    if (offset_width != 0) {
+        add_width += offset_width + 3;
+    }
+    if ((flags & HEXA) && (flags & (BINARY | ASCII))) {
+        add_width += 2;
+    }
+    if ((flags & BINARY) && (flags & ASCII)) {
+        add_width += 2;
+    }
+
+    // Computes max number of dumped bytes per line
+    size_type bytes_per_line;
+
+    if (flags & BPL) {
+        bytes_per_line = line_width;
+    }
+    else if (add_width >= line_width) {
+        bytes_per_line = 8;  // arbitrary, if indent is too long
+    }
+    else {
+        bytes_per_line = (line_width - add_width) /
+            (((flags & HEXA) ? (hexa_width + 1) : 0) +
+             ((flags & BINARY) ? (bin_width + 1) : 0) +
+             ((flags & ASCII) ? 1 : 0));
+        if (bytes_per_line > 1) {
+            bytes_per_line = bytes_per_line & ~1; // force even value
+        }
+    }
+    if (bytes_per_line == 0) {
+        bytes_per_line = 8;  // arbitrary, if ended up with none
+    }
+
+    // Display data
+    for (size_type line = 0; line < size; line += bytes_per_line) {
+
+        // Number of bytes on this line (last line may be shorter)
+        size_type line_size = line + bytes_per_line <= size ? bytes_per_line : size - line;
+
+        // Beginning of line
+        append(indent, u' ');
+        if (flags & OFFSET) {
+            append(Hexa(init_offset + line, offset_width, UString(), false, true));
+            append(u":  ");
+        }
+        append(inner_indent, u' ');
+
+        // Hexa dump
+        if (flags & HEXA) {
+            for (size_type byte = 0; byte < line_size; byte++) {
+                append(byte_prefix);
+                append(Hexa(raw[line + byte], 0, UString(), false, true));
+                append(byte_suffix);
+                if (byte < bytes_per_line - 1) {
+                    append(1, u' ');
+                }
+            }
+            if (flags & (BINARY | ASCII)) { // more to come
+                if (line_size < bytes_per_line) {
+                    append((hexa_width + 1) * (bytes_per_line - line_size) - 1, u' ');
+                }
+                append(2, u' ');
+            }
+        }
+
+        // Binary dump
+        if (flags & BINARY) {
+            for (size_type byte = 0; byte < line_size; byte++) {
+                int b = int(raw[line + byte]);
+                for (int i = 7; i >= 0; i--) {
+                    append(1, u'0' + UChar((b >> i) & 0x01));
+                    if (i == 4 && (flags & BIN_NIBBLE) != 0) {
+                        append(1, u'.');
+                    }
+                }
+                if (byte < bytes_per_line - 1) {
+                    append(1, u' ');
+                }
+            }
+            if (flags & ASCII) { // more to come
+                if (line_size < bytes_per_line) {
+                    append((bin_width + 1) * (bytes_per_line - line_size) - 1, u' ');
+                }
+                append(2, u' ');
+            }
+        }
+
+        // ASCII dump
+        if (flags & ASCII) {
+            for (size_type byte = 0; byte < line_size; byte++) {
+                // Display only ASCII characters. Other encodings don't make sense on one bytes.
+                const UChar c = UChar(raw[line + byte]);
+                push_back(c >= 0x20 && c <= 0x7E ? c : u'.');
+            }
+        }
+
+        // Insert a new-line, cleanup spurious spaces.
+        while (!empty() && back() == u' ') {
+            pop_back();
+        }
+        push_back(u'\n');
+    }
+}
+
+
+//----------------------------------------------------------------------------
 // Convert a DVB string into UTF-16.
 //----------------------------------------------------------------------------
 
-ts::UString ts::UString::FromDVB(const uint8_t* dvb, size_t dvbSize, const DVBCharset* charset)
+ts::UString ts::UString::FromDVB(const uint8_t* dvb, size_type dvbSize, const DVBCharset* charset)
 {
     // Null or empty buffer is a valid empty string.
     if (dvb == 0 || dvbSize == 0) {
@@ -825,7 +1060,7 @@ ts::UString ts::UString::FromDVB(const uint8_t* dvb, size_t dvbSize, const DVBCh
 
     // Get the DVB character set code from the beginning of the string.
     uint32_t code = 0;
-    size_t codeSize = 0;
+    size_type codeSize = 0;
     if (!DVBCharset::GetCharCodeTable(code, codeSize, dvb, dvbSize)) {
         return UString();
     }
@@ -842,7 +1077,7 @@ ts::UString ts::UString::FromDVB(const uint8_t* dvb, size_t dvbSize, const DVBCh
     if (charset == 0) {
         // Unsupported charset. Collect all ANSI characters, replace others by '.'.
         UString str(dvbSize, FULL_STOP);
-        for (size_t i = 0; i < dvbSize; i++) {
+        for (size_type i = 0; i < dvbSize; i++) {
             if (dvb[i] >= 0x20 && dvb[i] <= 0x7E) {
                 str[i] = UChar(dvb[i]);
             }
@@ -871,7 +1106,7 @@ ts::UString ts::UString::FromDVBWithByteLength(const uint8_t*& buffer, size_t& s
 
     // Address and size of the DVB string.
     const uint8_t* const dvb = buffer + 1;
-    const size_t dvbSize = std::min<size_t>(buffer[0], size - 1);
+    const size_type dvbSize = std::min<size_t>(buffer[0], size - 1);
 
     // Update the user buffer to point after the DVB string.
     buffer += dvbSize + 1;
@@ -886,7 +1121,7 @@ ts::UString ts::UString::FromDVBWithByteLength(const uint8_t*& buffer, size_t& s
 // Convert a UTF-16 string into DVB representation.
 //----------------------------------------------------------------------------
 
-size_t ts::UString::toDVB(uint8_t*& buffer, size_t& size, size_t start, size_t count, const DVBCharset* charset) const
+ts::UString::size_type ts::UString::toDVB(uint8_t*& buffer, size_t& size, size_type start, size_type count, const DVBCharset* charset) const
 {
     // Skip degenerated cases where there is nothing to do.
     if (buffer == 0 || size == 0 || start >= length()) {
@@ -903,7 +1138,7 @@ size_t ts::UString::toDVB(uint8_t*& buffer, size_t& size, size_t start, size_t c
 
     // Look for a character set which can encode the string.
     if (charset == 0 || !charset->canEncode(*this, start, count)) {
-        for (size_t i = 0; dvbEncoders[i] != 0; ++i) {
+        for (size_type i = 0; dvbEncoders[i] != 0; ++i) {
             if (dvbEncoders[i]->canEncode(*this, start, count)) {
                 charset = dvbEncoders[i];
                 break;
@@ -924,7 +1159,7 @@ size_t ts::UString::toDVB(uint8_t*& buffer, size_t& size, size_t start, size_t c
 // Convert a UTF-16 string into DVB representation in a byte block.
 //----------------------------------------------------------------------------
 
-ts::ByteBlock ts::UString::toDVB(size_t start, size_t count, const DVBCharset* charset) const
+ts::ByteBlock ts::UString::toDVB(size_type start, size_type count, const DVBCharset* charset) const
 {
     if (start >= length()) {
         return ByteBlock();
@@ -935,7 +1170,7 @@ ts::ByteBlock ts::UString::toDVB(size_t start, size_t count, const DVBCharset* c
 
         // Convert the string.
         uint8_t* buffer = bb.data();
-        size_t size = bb.size();
+        size_type size = bb.size();
         toDVB(buffer, size, start, count, charset);
 
         // Truncate unused bytes.
@@ -950,7 +1185,7 @@ ts::ByteBlock ts::UString::toDVB(size_t start, size_t count, const DVBCharset* c
 // Convert a UTF-16 string into DVB (preceded by its one-byte length).
 //----------------------------------------------------------------------------
 
-size_t ts::UString::toDVBWithByteLength(uint8_t*& buffer, size_t& size, size_t start, size_t count, const DVBCharset* charset) const
+ts::UString::size_type ts::UString::toDVBWithByteLength(uint8_t*& buffer, size_t& size, size_type start, size_type count, const DVBCharset* charset) const
 {
     // Skip degenerated cases where there is nothing to do.
     if (buffer == 0 || size == 0 || start >= length()) {
@@ -961,11 +1196,11 @@ size_t ts::UString::toDVBWithByteLength(uint8_t*& buffer, size_t& size, size_t s
     uint8_t* dvbBuffer = buffer + 1;
 
     // We cannot write more that 255 bytes because the length must fit in one byte.
-    const size_t dvbMaxSize = std::min<size_t>(size - 1, 0xFF);
-    size_t dvbSize = dvbMaxSize;
+    const size_type dvbMaxSize = std::min<size_t>(size - 1, 0xFF);
+    size_type dvbSize = dvbMaxSize;
 
     // Convert the string.
-    const size_t result = toDVB(dvbBuffer, dvbSize, start, count, charset);
+    const size_type result = toDVB(dvbBuffer, dvbSize, start, count, charset);
 
     // Compute the actual DVB size.
     assert(dvbSize <= dvbMaxSize);
@@ -988,7 +1223,7 @@ size_t ts::UString::toDVBWithByteLength(uint8_t*& buffer, size_t& size, size_t s
 // Encode this UTF-16 string into a DVB string (preceded by its one-byte length).
 //----------------------------------------------------------------------------
 
-ts::ByteBlock ts::UString::toDVBWithByteLength(size_t start, size_t count, const DVBCharset* charset) const
+ts::ByteBlock ts::UString::toDVBWithByteLength(size_type start, size_type count, const DVBCharset* charset) const
 {
     if (start >= length()) {
         // Empty string, return one byte containing 0 (the length).
@@ -1000,7 +1235,7 @@ ts::ByteBlock ts::UString::toDVBWithByteLength(size_t start, size_t count, const
 
         // Convert the string.
         uint8_t* buffer = bb.data() + 1;
-        size_t size = bb.size() - 1;
+        size_type size = bb.size() - 1;
         toDVB(buffer, size, start, count, charset);
 
         // Truncate unused bytes.
