02920e0406b0b58a47d06012a4e97110d8fa0e23
Thierry Lelegard <thierry@lelegard.fr>
2017-12-13 23:31:54 +0100

    Fixed git hook

--- build/git-hook.sh
+++ build/git-hook.sh
@@ -45,6 +45,8 @@ SRCFILE="$ROOTDIR/src/libtsduck/tsVersion.h"
 PREFIX="#define TS_COMMIT"
 
 # Use GNU variants of sed and grep when available.
+# Add /usr/local/bin at end of PATH in case we can find them there.
+export PATH=PATH:/usr/local/bin
 [[ -n "$(which gsed 2>/dev/null)" ]] && sed() { gsed "$@"; }
 [[ -n "$(which ggrep 2>/dev/null)" ]] && grep() { ggrep "$@"; }
 
