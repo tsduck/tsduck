168fa7537df30fbe42a2f68d377efe0be8fea204
Thierry Lelegard <thierry@lelegard.fr>
2018-03-25 23:52:42 +0200

    Added script to build TSDuck installers on a remote host or virtual machine

--- build/build-remote.sh
+++ build/build-remote.sh
@@ -38,7 +38,6 @@
 
 SCRIPT=$(basename $BASH_SOURCE)
 ROOTDIR=$(cd $(dirname $BASH_SOURCE)/..; pwd)
-INSTDIR="$ROOTDIR/installers"
 
 error() { echo >&2 "$SCRIPT: $*"; exit 1; }
 usage() { echo >&2 "invalid command, try \"$SCRIPT --help\""; exit 1; }
@@ -50,6 +49,8 @@ REMOTE_WIN=false
 USER_NAME=$(id -un)
 HOST_NAME=
 VMX_FILE=
+SSH_TIMEOUT=5
+BOOT_TIMEOUT=120
 
 
 #-----------------------------------------------------------------------------
@@ -77,6 +78,10 @@ Options:
   -h name
   --host name
       Build on this remote host. Mandatory for remote hosts. Optional for VM's.
+
+  -t seconds
+  --timeout seconds
+      Connexion timeout for ssh and scp. Default: $SSH_TIMEOUT seconds.
 
   -u name
   --user name
@@ -113,6 +118,10 @@ while [[ $# -gt 0 ]]; do
             [[ $# -gt 1 ]] || usage; shift
             HOST_NAME=$1
             ;;
+        -t|--timeout)
+            [[ $# -gt 1 ]] || usage; shift
+            SSH_TIMEOUT=$1
+            ;;
         -u|--user)
             [[ $# -gt 1 ]] || usage; shift
             USER_NAME=$1
@@ -136,12 +145,98 @@ done
 # Remote build.
 #-----------------------------------------------------------------------------
 
+SSH_OPTS="-o ConnectTimeout=$SSH_TIMEOUT"
+NAME="$HOST_NAME"
+
 curdate()  { date +%Y%m%d-%H%M; }
-testping() { ping -o -q -t 2 "$1" &>/dev/null; }
 
+# Process VMWare startup.
+VMX_SHUTDOWN=false
+if [[ -n "$VMX_FILE" ]]; then
+
+    # Locate vmrun, the VMWare command line.
+    # Try in PATH and then VMWare Fusion installation path on macOS.
+    VMRUN=$(which vmrun 2>/dev/null)
+    [[ -z "$VMRUN" ]] && VMRUN=$(ls "/Applications/VMware Fusion.app/Contents/Public/vmrun" 2>/dev/null)
+    [[ -z "$VMRUN" ]] && error "vmrun not found, cannot manage VMWare VM's"
+
+    # Name for log file.
+    [[ -z "$HOST_NAME" ]] && NAME=$(basename "$VMX_FILE" .vmx | tr A-Z a-z)
+
+    # Try to get IP address of VM.
+    IP=$("$VMRUN" getGuestIPAddress "$VMX_FILE")
+    if [[ $? -ne 0 ]]; then
+
+        # Cannot get VM IP address, try to boot the VM.
+        echo Booting $(basename "$VMX_FILE")
+        "$VMRUN" start "$VMX_FILE" nogui || error "cannot start VM in $VMX_FILE"
+
+        # Wait until we can get the IP address of the VM.
+        if [[ -z "$HOST_NAME" ]]; then
+            maxdate=$(( $(date +%s) + $BOOT_TIMEOUT ))
+            ok=1
+            while [[ $(date +%s) -lt $maxdate ]]; do
+                IP=$("$VMRUN" getGuestIPAddress "$VMX_FILE")
+                ok=$?
+                [[ $ok -eq 0 ]] && break
+                sleep 5
+            done
+            [[ $ok -ne 0 ]] && error "Cannot get IP address of $HOST_NAME after $BOOT_TIMEOUT seconds"
+            echo "IP address for $NAME is $IP, trying to ssh..."
+            HOST_NAME="$IP"
+        fi
+
+        # Wait that the machine is accessible using ssh.
+        ssh -o ConnectTimeout=$BOOT_TIMEOUT "$HOST_NAME" cd &>/dev/null || error "cannot contact VM $BOOT_TIMEOUT seconds after boot, aborting"
+        echo "SSH ok for $HOST_NAME"
+
+        # We need to shutdown the VM after building the installers.
+        VMX_SHUTDOWN=true
+    fi
+
+    # Use the IP address if no host name is provided.
+    [[ -z "$HOST_NAME" ]] && HOST_NAME="$IP"
+fi
+
+# Check accessibility of remote host.
 [[ -z "$HOST_NAME" ]] && error "no remote host specified"
-testping "$HOST_NAME" || error "$HOST_NAME not responding"
+ssh $SSH_OPTS "$HOST_NAME" cd &>/dev/null || error "$HOST_NAME not responding"
 
-ssh "$USER_NAME@$HOST_NAME" make -C "$REMOTE_DIR" installer &>"$INSTDIR/build-${HOST_NAME}-$(curdate).log"
+# Build remote installers.
+(
+    if $WINDOWS; then
+        # Build on Windows.
+        ssh $SSH_OPTS "$USER_NAME@$HOST_NAME" \
+            PowerShell "$REMOTE_DIR\\installers\\Build-Installer.ps1"
+
+        # @@@ TODO: Detect new installers and get them
+
+    else
+        # Build on Unix.
+        # Create a remote timestamp. Newer files are the installers we build.
+        # Build installers from scratch after updating the repository.
+        ssh $SSH_OPTS "$USER_NAME@$HOST_NAME" \
+            cd "$REMOTE_DIR" '&&' \
+            git pull '&&' \
+            make distclean ';' \
+            touch "installers/timestamp.tmp" '&&' \
+            make installer
+
+        # Copy all files from installers directory which are newer than the time stamp.
+        for f in $(ssh $SSH_OPTS "$USER_NAME@$HOST_NAME" find "$REMOTE_DIR/installers" -type f -newer "$REMOTE_DIR/installers/timestamp.tmp"); do
+            scp $SSH_OPTS "$USER_NAME@$HOST_NAME:$f" "$ROOTDIR/installers/"
+        done
+
+        # Delete the temporary timestamp.
+        ssh $SSH_OPTS "$USER_NAME@$HOST_NAME" rm -f "$REMOTE_DIR/installers/timestamp.tmp"
+    fi
+) &>"$ROOTDIR/installers/build-${HOST_NAME}-$(curdate).log"
+
+
+# Shutdown the VM if we booted it.
+if $VMX_SHUTDOWN; then
+    echo "Shutting down" $(basename "$VMX_FILE")
+    "$VMRUN" stop "$VMX_FILE" soft
+fi
 
 exit 0
