cfe9a0f4eb79cab7018fb2387e50bc77cd3a4803
Thierry Lelegard <thierry@lelegard.fr>
2018-06-04 22:11:52 +0200

    Added options --udp and --unregulated to datainject

--- src/libtsduck/tsUDPSocket.cpp
+++ src/libtsduck/tsUDPSocket.cpp
@@ -372,8 +372,15 @@ bool ts::UDPSocket::receive(void* data,
         // Wait for a message.
         const SocketErrorCode err = receiveOne(data, max_size, ret_size, sender, destination, report);
 
-        if (err == SYS_SUCCESS) {
-            return true;
+        if (abort != 0 && abort->aborting()) {
+            // Aborting, no error message.
+            return false;
+        }
+        else if (err == SYS_SUCCESS) {
+            // Sometimes, we get "successful" empty message coming from nowhere. Ignore them.
+            if (ret_size > 0 || sender.hasAddress()) {
+                return true;
+            }
         }
         else if (abort != 0 && abort->aborting()) {
             // User-interrupt, end of processing but no error message
