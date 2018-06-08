91bc11475481e0166a996e8b293ed6eb438a7ba5
Thierry Lelegard <thierry@lelegard.fr>
2018-06-08 23:03:02 +0200

    Updated release text for 3.12

--- src/libtsduck/tsVersion.h
+++ src/libtsduck/tsVersion.h
@@ -40,8 +40,8 @@
 //!
 //! TSDuck minor version.
 //!
-#define TS_VERSION_MINOR 12
+#define TS_VERSION_MINOR 13
 //!
 //! TSDuck commit number (automatically updated by Git hooks).
 //!
-#define TS_COMMIT 781
+#define TS_COMMIT 782
