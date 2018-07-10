e7112e44bd86a662288ae6bf8aaca198121fa531
Thierry Lelegard <thierry@lelegard.fr>
2018-07-10 23:12:26 +0200

    Added Windows build scripts for the sample plugin

---
+++ sample/sample-plugin/test-sample.ps1
@@ -0,0 +1,15 @@
+﻿# PowerShell script to test the sample plugin on Windows.
+
+# Directory of this script.
+$dir = $PSScriptRoot
+
+# We assume that the plugin DLL has been copied into the same
+# directory as the script (see build.ps1 for instance).
+$env:TSPLUGINS_PATH = "${dir};${env:TSPLUGINS_PATH}"
+
+# Test the sample plugin.
+tsp -I null -P until --packet 200 -P sample --count -O drop
+
+# WARNING: tsp writes its log on stderr which makes PowerShell interpret
+# this log as error. So, you have to ignore the extra error messages
+# from PowerShell, claiming that tsp generated errors.
