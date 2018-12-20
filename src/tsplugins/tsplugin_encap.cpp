3d2ff8859214b230d6e4911aede4b781ac51e147
lars18th <dani@Internet-THINK.localdomain>
2018-12-20 16:10:00 +0100

    [v2] PES mode Encapsulation

--- src/tsplugins/tsplugin_encap.cpp
+++ src/tsplugins/tsplugin_encap.cpp
@@ -56,10 +56,12 @@ namespace ts {
     private:
         bool                _ignoreErrors;  // Ignore encapsulation errors.
         bool                _pack;          // Outer packet packing option.
+        size_t              _packLimit;     // Max limit distance.
         size_t              _maxBuffered;   // Max buffered packets.
         PID                 _pidOutput;     // Output PID.
         PID                 _pidPCR;        // PCR reference PID.
         PIDSet              _pidsInput;     // Input PID's.
+        size_t              _modePES;       // Enable PES mode and select type.
         PacketEncapsulation _encap;         // Encapsulation engine.
 
         // Inaccessible operations
@@ -81,10 +83,12 @@ ts::EncapPlugin::EncapPlugin(TSP* tsp_) :
     ProcessorPlugin(tsp_, u"Encapsulate packets from several PID's into one single PID", u"[options]"),
     _ignoreErrors(false),
     _pack(false),
+    _packLimit(0),
     _maxBuffered(0),
     _pidOutput(PID_NULL),
     _pidPCR(PID_NULL),
     _pidsInput(),
+    _modePES(0),
     _encap()
 {
     option(u"ignore-errors", 'i');
@@ -112,18 +116,27 @@ ts::EncapPlugin::EncapPlugin(TSP* tsp_) :
          u"Specify a reference PID containing PCR's. The output PID will contain PCR's, "
          u"based on the same clock. By default, the output PID does not contain any PCR.");
 
-    option(u"pack");
+    option(u"pack", 0, INTEGER, 0, 1, 0, UNLIMITED_VALUE);
     help(u"pack",
          u"Emit outer packets when they are full only. By default, emit outer packets "
          u"as soon as possible, when null packets are available on input. With the default "
          u"behavior, inner packets are decapsulated with a better time accuracy, at the expense "
-         u"of a higher bitrate of the outer PID when there are many null packets in input.");
+         u"of a higher bitrate of the outer PID when there are many null packets in input. "
+         u"You can limit the distance between packets adding a positive value. "
+         u"With a 0 value the distance is disabled (=unlimited). "
+         u"The value 1 is equivalent to not use the pack mode.");
 
     option(u"pid", 'p', INTEGER, 1, UNLIMITED_COUNT, 0, PID_NULL - 1);
     help(u"pid", u"pid1[-pid2]",
          u"Specify an input PID or range of PID's to encapsulate. "
          u"Several --pid options can be specified. "
          u"The null PID 0x1FFF cannot be encapsulated.");
+
+    option(u"pes-mode", 0, INTEGER, 0, 1, 0, 2);
+    help(u"pes-mode", u"mode",
+         u"Enable PES mode encapsulation. "
+         u"Modes available are: "
+         u"0=disabled; 1=fixed; 2=variable.");
 }
 
 
@@ -135,9 +148,11 @@ bool ts::EncapPlugin::getOptions()
 {
     _ignoreErrors = present(u"ignore-errors");
     _pack = present(u"pack");
+    _packLimit = intValue<size_t>(u"pack", 0);
     _maxBuffered = intValue<size_t>(u"max-buffered-packets", PacketEncapsulation::DEFAULT_MAX_BUFFERED_PACKETS);
     _pidOutput = intValue<PID>(u"output-pid", PID_NULL);
     _pidPCR = intValue<PID>(u"pcr-pid", PID_NULL);
+    _modePES = intValue<size_t>(u"pes-mode", 0);
     getIntValues(_pidsInput, u"pid");
 
     return true;
@@ -151,7 +166,8 @@ bool ts::EncapPlugin::getOptions()
 bool ts::EncapPlugin::start()
 {
     _encap.reset(_pidOutput, _pidsInput, _pidPCR);
-    _encap.setPacking(_pack);
+    _encap.setPacking(_pack, _packLimit);
+    _encap.setPES(_modePES);
     _encap.setMaxBufferedPackets(_maxBuffered);
     return true;
 }
