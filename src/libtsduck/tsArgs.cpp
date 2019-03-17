ddc719d330c07357522b6555e4d7fde4133d67f7
Thierry Lelegard <thierry@lelegard.fr>
2019-03-17 13:46:11 +0100

    In plugin bitrate_monitor, allow to monitor the complete TS

--- src/libtsduck/tsArgs.cpp
+++ src/libtsduck/tsArgs.cpp
@@ -346,16 +346,19 @@ ts::UString ts::Args::formatHelpOptions(size_t line_width) const
         const IOption& opt(it->second);
         if (opt.name.empty()) {
             // This is the parameters (ie. not options).
-            if (!text.empty()) {
+            // Print nothing if parameters are undocumented.
+            if (!opt.help.empty() || !opt.syntax.empty()) {
+                if (!text.empty()) {
+                    text += LINE_FEED;
+                }
+                UString title(u"Parameter");
+                if (opt.max_occur > 1) {
+                    title += u's';
+                }
+                text += HelpLines(0, title + u':', line_width);
                 text += LINE_FEED;
+                text += HelpLines(1, opt.help.empty() ? opt.syntax : opt.help, line_width);
             }
-            UString title(u"Parameter");
-            if (opt.max_occur > 1) {
-                title += u's';
-            }
-            text += HelpLines(0, title + u':', line_width);
-            text += LINE_FEED;
-            text += HelpLines(1, opt.help.empty() ? opt.syntax : opt.help, line_width);
         }
         else {
             // This is an option. Add 'Options:' the first time.
--- src/libtsduck/tsVersion.h
+++ src/libtsduck/tsVersion.h
@@ -44,4 +44,4 @@
 //!
 //! TSDuck commit number (automatically updated by Git hooks).
 //!
-#define TS_COMMIT 1171
+#define TS_COMMIT 1172
