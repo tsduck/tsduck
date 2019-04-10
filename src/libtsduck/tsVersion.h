fc5b5b4d3b18cb97d9f7e1f6c92916be60d6e6f7
Thierry Lelegard <thierry@lelegard.fr>
2019-04-10 22:21:18 +0200

    Switched to version 3.18

--- src/libtsduck/tsVersion.h
+++ src/libtsduck/tsVersion.h
@@ -40,8 +40,8 @@
 //!
 //! TSDuck minor version.
 //!
-#define TS_VERSION_MINOR 17
+#define TS_VERSION_MINOR 18
 //!
 //! TSDuck commit number (automatically updated by Git hooks).
 //!
-#define TS_COMMIT 1189
+#define TS_COMMIT 1190
