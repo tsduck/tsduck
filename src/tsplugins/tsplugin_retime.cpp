0423d0fd037f78d9646faeb7f970b10b4709d477
Lars The <lars18th@users.noreply.github.com>
2019-07-02 15:56:26 +0200

    Update tsplugin_retime.cpp

--- src/tsplugins/tsplugin_retime.cpp
+++ src/tsplugins/tsplugin_retime.cpp
@@ -2,7 +2,7 @@
 //
 // TSDuck - The MPEG Transport Stream Toolkit
 // Copyright (c) 2005-2019, Thierry Lelegard
-// Copyright (c) 2019, lars18th
+// Copyright (c) 2019, Lars The (lars18th)
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
