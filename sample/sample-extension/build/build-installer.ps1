486fff473efc47cc8be29c49c185817cc4474952
Thierry Lelegard <thierry@lelegard.fr>
2019-09-02 00:44:31 +0200

    Completed sample extension on Windows

---
+++ sample/sample-extension/build/build-installer.ps1
@@ -0,0 +1,57 @@
+﻿# Build the TSDuck extension binary installers for Windows.
+
+param([switch]$NoPause = $false)
+
+$RootDir = (Split-Path -Parent $PSScriptRoot)
+$MsvcDir = (Join-Path $RootDir "msvc")
+$SrcDir = (Join-Path $RootDir "src")
+$InstallerDir = (Join-Path $RootDir "installers")
+$Script = (Join-Path $PSScriptRoot "foo.nsi")
+
+# Lower process priority so that the build does not eat up all CPU.
+(Get-Process -Id $PID).PriorityClass = "BelowNormal"
+
+# Exit this script. If optional variable $NoPause is $false, pause.
+function Exit-Script
+{
+    param([Parameter(Mandatory=$false)][String] $Message = "", [switch]$NoPause = $false)
+    $Code = 0
+    if ($Message -ne "") {
+        Write-Host "ERROR: $Message"
+        $Code = 1
+    }
+    if (-not $NoPause) {
+        pause
+    }
+    exit $Code
+}
+
+# Locate NSIS, the Nullsoft Scriptable Installation System.
+# Find MSBuild.exe.
+$NSIS = ""
+$NSISPath = ($env:Path -split ";") + @('C:\Program Files\NSIS', 'C:\Program Files (x86)\NSIS') | Where-Object {$_}
+foreach ($dir in $NSISPath) {
+    $path = (Join-Path $dir "makensis.exe")
+    if (Test-Path $path) {
+        $NSIS = $path
+        break
+    }
+}
+if (-not $NSIS) {
+    Exit-Script -NoPause:$NoPause "NSIS not found"
+}
+
+# Build the project.
+Push-Location
+& (Join-Path $PSScriptRoot build.ps1) -NoPause
+$Code = $LastExitCode
+Pop-Location
+if ($Code -ne 0) {
+    Exit-Script -NoPause:$NoPause "Error building the project"
+}
+
+# Build binary installers.
+& $NSIS /V2 /DWin64 $Script
+& $NSIS /V2 /DWin32 $Script
+
+Exit-Script -NoPause:$NoPause
---
+++ sample/sample-extension/build/cleanup.ps1
@@ -0,0 +1,54 @@
+﻿# Windows PowerShell script to clean up the project directory tree.
+# Back to a clean state of source files.
+
+[CmdletBinding(SupportsShouldProcess=$true)]
+param([switch]$NoPause = $false)
+
+$RootDir = (Split-Path -Parent $PSScriptRoot)
+
+# Note that we cannot pipe Get-ChildItem directly into Remove-Item since Get-ChildItem -Recurse
+# return directories first, followed by their content. So, we first get the list of files and
+# directories to remove and then we delete them, if not yet deleted by a previous recursion.
+
+$files = @(Get-ChildItem -Recurse -Force $RootDir | Where-Object {
+    ($_.FullName -notlike '*\.git\*') -and
+    ($_.FullName -notlike '*\installers\*') -and
+    ($_.Name -like "doxy" -or
+    $_.Name -like "debug" -or
+    $_.Name -like "debug-*" -or
+    $_.Name -like "release" -or
+    $_.Name -like "release-*" -or
+    $_.Name -like "ipch" -or
+    $_.Name -like ".vs" -or
+    $_.Name -like "*.user" -or
+    $_.Name -like "*.user.*" -or
+    $_.Name -like "*.VC.db" -or
+    $_.Name -like "*.VC.opendb" -or
+    $_.Name -like "*.sdf" -or
+    $_.Name -like "*.suo" -or
+    $_.Name -like "*.opensdf" -or
+    $_.Name -like "*~" -or
+    $_.Name -like "*.exe" -or
+    $_.Name -like "*.obj" -or
+    $_.Name -like "*.o" -or
+    $_.Name -like "*.so" -or
+    $_.Name -like "core" -or
+    $_.Name -like "core.*" -or
+    $_.Name -like "*.bak" -or
+    $_.Name -like "*.tmp" -or
+    $_.Name -like "*.lib" -or
+    $_.Name -like "*.dll" -or
+    $_.Name -like "*.exe" -or
+    $_.Name -like "*.autosave")
+}).FullName
+
+foreach ($file in $files) {
+    if ((Test-Path $file) -and $PSCmdlet.ShouldProcess($file,"Delete")) {
+        "Deleting $file"
+        Remove-Item $file -Recurse -Force
+    }
+}
+
+if (-not $NoPause) {
+    pause
+}
