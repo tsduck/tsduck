ab73e4841fe3bbbdbb0f3060ace9845e32cd6c85
Thierry Lelegard <thierry@lelegard.fr>
2019-09-30 22:19:55 +0200

    Completed DCCSCT

--- src/libtsduck/tsDCCSCT.cpp
+++ src/libtsduck/tsDCCSCT.cpp
@@ -245,7 +245,36 @@ void ts::DCCSCT::serializeContent(DuckContext& duck, BinaryTable& table) const
         uint8_t* data_length_addr = data + 1;
         data += 2; remain -= 2;
 
-        // @@@@@@@@@@@@@@@@@
+        // We always need at least 4 bytes for the length of the two descriptor lists.
+        if (remain < 4) {
+            return;
+        }
+
+        // Insert type-dependent data. We now know that we have enough space for the fixed parts.
+        switch (upd.update_type) {
+            case new_genre_category: {
+                data[0] = upd.genre_category_code;
+                data++; remain--;
+                upd.genre_category_name_text.serialize(duck, data, remain);
+                break;
+            }
+            case new_state: {
+                data[0] = upd.dcc_state_location_code;
+                data++; remain--;
+                upd.dcc_state_location_code_text.serialize(duck, data, remain);
+                break;
+            }
+            case new_county: {
+                data[0] = upd.state_code;
+                PutUInt16(data + 1, 0xFC00 | upd.dcc_county_location_code);
+                data += 3; remain -= 3;
+                upd.dcc_county_location_code_text.serialize(duck, data, remain);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
 
         // Update update_data_length
         *data_length_addr = uint8_t(data - data_length_addr - 1);
@@ -254,8 +283,6 @@ void ts::DCCSCT::serializeContent(DuckContext& duck, BinaryTable& table) const
         size_t next_index = upd.descs.lengthSerialize(data, remain, 0, 0x003F, 10);
         if (next_index < upd.descs.count()) {
             // Not enough space to serialize all descriptors in the section.
-            // A DCCSCT cannot have more than one section.
-            // Return with table left in invalid state.
             return;
         }
     }
@@ -264,8 +291,6 @@ void ts::DCCSCT::serializeContent(DuckContext& duck, BinaryTable& table) const
     size_t next_index = descs.lengthSerialize(data, remain, 0, 0x003F, 10);
     if (next_index < descs.count()) {
         // Not enough space to serialize all descriptors in the section.
-        // A DCCSCT cannot have more than one section.
-        // Return with table left in invalid state.
         return;
     }
 
@@ -288,12 +313,91 @@ void ts::DCCSCT::serializeContent(DuckContext& duck, BinaryTable& table) const
 
 void ts::DCCSCT::DisplaySection(TablesDisplay& display, const ts::Section& section, int indent)
 {
-    //@@ std::ostream& strm(display.duck().out());
-    //@@ const std::string margin(indent, ' ');
+    std::ostream& strm(display.duck().out());
+    const std::string margin(indent, ' ');
     const uint8_t* data = section.payload();
     size_t size = section.payloadSize();
 
-    // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    if (size >= 2) {
+        // Fixed part.
+        size_t updates_defined = data[1];
+        strm << margin << UString::Format(u"Protocol version: %d, DCCSCT type: 0x%X, number of updates: %d", {data[0], section.tableIdExtension(), updates_defined}) << std::endl;
+        data += 2; size -= 2;
+
+        // Loop on all update.
+        while (updates_defined > 0 && size >= 2) {
+
+            const uint8_t utype = data[0];
+            size_t len = data[1];
+            data += 2; size -= 2;
+
+            strm << margin << UString::Format(u"- Update type: 0x%X (%s)", {utype, UpdateTypeNames.name(utype)}) << std::endl;
+
+            if (size < len) {
+                break;
+            }
+
+            // Display variable part.
+            switch (utype) {
+                case new_genre_category: {
+                    if (len >= 1) {
+                        strm << margin << UString::Format(u"  Genre category code: 0x%X (%d)", {data[0], data[0]}) << std::endl;
+                        data++; size--;
+                        ATSCMultipleString::Display(display, u"Genre category name: ", indent + 2, data, size, len - 1);
+                    }
+                    break;
+                }
+                case new_state: {
+                    if (len >= 1) {
+                        strm << margin << UString::Format(u"  DCC state location code: 0x%X (%d)", {data[0], data[0]}) << std::endl;
+                        data++; size--;
+                        ATSCMultipleString::Display(display, u"DCC state location: ", indent + 2, data, size, len - 1);
+                    }
+                    break;
+                }
+                case new_county: {
+                    if (len >= 3) {
+                        const uint16_t county = GetUInt16(data + 1) & 0x03FF;
+                        strm << margin << UString::Format(u"  State code: 0x%X (%d), DCC county location code: 0x%03X (%d)", {data[0], data[0], county, county}) << std::endl;
+                        data += 3; size -= 3;
+                        ATSCMultipleString::Display(display, u"DCC county location: ", indent + 2, data, size, len - 3);
+                    }
+                    break;
+                }
+                default: {
+                    display.displayExtraData(data, len, indent + 2);
+                    data += len; size -= len;
+                    break;
+                }
+            }
+
+            // Display descriptor list for this update.
+            if (size >= 2) {
+                len = GetUInt16(data) & 0x03FF;
+                data += 2; size -= 2;
+                len = std::min(len, size);
+                if (len > 0) {
+                    strm << margin << "  Descriptors for this update:" << std::endl;
+                    display.displayDescriptorList(section, data, len, indent + 2);
+                    data += len; size -= len;
+                }
+            }
+
+            updates_defined--;
+        }
+
+        // Display descriptor list for the global table.
+        if (size >= 2) {
+            size_t len = GetUInt16(data) & 0x03FF;
+            data += 2; size -= 2;
+            len = std::min(len, size);
+            if (len > 0) {
+                strm << margin << "Additional descriptors:" << std::endl;
+                display.displayDescriptorList(section, data, len, indent);
+                data += len; size -= len;
+            }
+        }
+    }
 
     display.displayExtraData(data, size, indent);
 }
@@ -358,10 +462,16 @@ void ts::DCCSCT::fromXML(DuckContext& duck, const xml::Element* element)
     for (size_t index = 0; _is_valid && index < children.size(); ++index) {
         // Add a new TableType at the end of the list.
         Update& upd(updates.newEntry());
-
+        xml::ElementVector unused;
         _is_valid =
             children[index]->getIntEnumAttribute(upd.update_type, UpdateTypeNames, u"update_type", true) &&
-            //@@@@@@@@@@@@@
-            upd.descs.fromXML(duck, children[index]);
+            children[index]->getIntAttribute<uint8_t>(upd.genre_category_code, u"genre_category_code", upd.update_type == new_genre_category) &&
+            children[index]->getIntAttribute<uint8_t>(upd.dcc_state_location_code, u"dcc_state_location_code", upd.update_type == new_state) &&
+            children[index]->getIntAttribute<uint8_t>(upd.state_code, u"state_code", upd.update_type == new_county) &&
+            children[index]->getIntAttribute<uint16_t>(upd.dcc_county_location_code, u"dcc_county_location_code", upd.update_type == new_county, 0, 0, 0x03FF) &&
+            upd.genre_category_name_text.fromXML(duck, children[index], u"genre_category_name_text", upd.update_type == new_genre_category) &&
+            upd.dcc_state_location_code_text.fromXML(duck, children[index], u"dcc_state_location_code_text", upd.update_type == new_state) &&
+            upd.dcc_county_location_code_text.fromXML(duck, children[index], u"dcc_county_location_code_text", upd.update_type == new_county) &&
+            upd.descs.fromXML(duck, unused, children[index], u"genre_category_name_text,dcc_state_location_code_text,dcc_county_location_code_text");
     }
 }
