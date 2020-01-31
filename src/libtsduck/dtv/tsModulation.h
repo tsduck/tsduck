bcbfc78728bad0f8e762241cfa32d9eb8bda216a
Lars The <lars18th@users.noreply.github.com>
2020-01-31 18:26:39 +0100

    Update Modulation values with old values
    In Kernel version 3.7.0 these values were added:
    - QAM_4_NR
    - FEC_2_5
    - TRANSMISSION_MODE_C1
    - TRANSMISSION_MODE_C3780
    - GUARD_INTERVAL_PN420
    - GUARD_INTERVAL_PN595
    - GUARD_INTERVAL_PN945

--- src/libtsduck/dtv/tsModulation.h
+++ src/libtsduck/dtv/tsModulation.h
@@ -108,7 +108,7 @@ namespace ts {
         APSK_16  = ::APSK_16,
         APSK_32  = ::APSK_32,
         DQPSK    = ::DQPSK,
-#if !defined(QAM_4_NR)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
         QAM_4_NR = -14,
 #else
         QAM_4_NR = ::QAM_4_NR,
@@ -201,11 +201,12 @@ namespace ts {
         FEC_3_5  = ::FEC_3_5,
         FEC_1_3  = -12,
         FEC_1_4  = -13,
-#if !defined(FEC_2_5)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
         FEC_2_5  = -14,
 #else
         FEC_2_5  = ::FEC_2_5,
 #endif
+        FEC_5_11 = -15,
         FEC_5_11 = -15,
 #elif defined(TS_WINDOWS) && !defined(DOXYGEN)
         FEC_NONE = ::BDA_BCC_RATE_NOT_SET,
@@ -404,14 +405,11 @@ namespace ts {
         TM_1K    = ::TRANSMISSION_MODE_1K,
         TM_16K   = ::TRANSMISSION_MODE_16K,
         TM_32K   = ::TRANSMISSION_MODE_32K,
-#if !defined(TRANSMISSION_MODE_C1)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
         TM_C1    = -12,
-#else
-        TM_C1    = ::TRANSMISSION_MODE_C1,
-#endif
-#if !defined(TRANSMISSION_MODE_C3780)
         TM_C3780 = -13,
 #else
+        TM_C1    = ::TRANSMISSION_MODE_C1,
         TM_C3780 = ::TRANSMISSION_MODE_C3780,
 #endif
 #elif defined(TS_WINDOWS) && !defined(DOXYGEN)
@@ -459,19 +457,13 @@ namespace ts {
         GUARD_1_128  = ::GUARD_INTERVAL_1_128,
         GUARD_19_128 = ::GUARD_INTERVAL_19_128,
         GUARD_19_256 = ::GUARD_INTERVAL_19_256,
-#if !defined(GUARD_INTERVAL_PN420)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
         GUARD_PN420  = -9,
-#else
-        GUARD_PN420  = ::GUARD_INTERVAL_PN420,
-#endif
-#if !defined(GUARD_INTERVAL_PN595)
         GUARD_PN595  = -10,
-#else
-        GUARD_PN595  = ::GUARD_INTERVAL_PN595,
-#endif
-#if !defined(GUARD_INTERVAL_PN945)
         GUARD_PN945  = -11,
 #else
+        GUARD_PN420  = ::GUARD_INTERVAL_PN420,
+        GUARD_PN595  = ::GUARD_INTERVAL_PN595,
         GUARD_PN945  = ::GUARD_INTERVAL_PN945,
 #endif
 #elif defined(TS_WINDOWS) && !defined(DOXYGEN)
