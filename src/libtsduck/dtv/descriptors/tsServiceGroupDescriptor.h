ed84e73c1ae7d6e0c2bfa96e49d1e9ff82b02051
Thierry Lelegard <thierry@lelegard.fr>
2020-05-13 22:33:14 +0200

    Added descriptor

---
+++ src/libtsduck/dtv/descriptors/tsServiceGroupDescriptor.h
@@ -0,0 +1,90 @@
+//----------------------------------------------------------------------------
+//
+// TSDuck - The MPEG Transport Stream Toolkit
+// Copyright (c) 2005-2020, Thierry Lelegard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+//----------------------------------------------------------------------------
+//!
+//!  @file
+//!  Representation of an ISDB service_group_descriptor.
+//!
+//----------------------------------------------------------------------------
+
+#pragma once
+#include "tsAbstractDescriptor.h"
+
+namespace ts {
+
+    //!
+    //! Representation of an ISDB service_group_descriptor.
+    //! @see ARIB STD-B10, Part 2, 6.2.49
+    //! @ingroup descriptor
+    //!
+    class TSDUCKDLL ServiceGroupDescriptor : public AbstractDescriptor
+    {
+    public:
+        //!
+        //! Simultaneous service entry.
+        //!
+        struct TSDUCKDLL SimultaneousService
+        {
+            SimultaneousService();          //!< Constructor.
+            uint16_t primary_service_id;    //!< Primary service id.
+            uint16_t secondary_service_id;  //!< Secondary service id.
+        };
+
+        //!
+        //!< List of simultaneous service entries.
+        //!
+        typedef std::list<SimultaneousService> SimultaneousServiceList;
+
+        // ServiceGroupDescriptor public members:
+        uint8_t                 service_group_type;     //!< 4 bits, group type.
+        SimultaneousServiceList simultaneous_services;  //!< List of simultaneous service, when service_group_type == 1.
+        ByteBlock               private_data;           //!< Private data for other group types.
+
+        //!
+        //! Default constructor.
+        //!
+        ServiceGroupDescriptor();
+
+        //!
+        //! Constructor from a binary descriptor
+        //! @param [in,out] duck TSDuck execution context.
+        //! @param [in] bin A binary descriptor to deserialize.
+        //!
+        ServiceGroupDescriptor(DuckContext& duck, const Descriptor& bin);
+
+        // Inherited methods
+        virtual void serialize(DuckContext&, Descriptor&) const override;
+        virtual void deserialize(DuckContext&, const Descriptor&) override;
+        virtual void fromXML(DuckContext&, const xml::Element*) override;
+        DeclareDisplayDescriptor();
+
+    protected:
+        // Inherited methods
+        virtual void buildXML(DuckContext&, xml::Element*) const override;
+    };
+}
