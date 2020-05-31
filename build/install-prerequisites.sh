1a7b737ff4b99fddc4923bef8a61e36c1406fdc2
Thierry Lelegard <thierry@lelegard.fr>
2020-05-31 22:38:54 +0200

    Fixed scripts for static and m32 builds

--- build/install-prerequisites.sh
+++ build/install-prerequisites.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #-----------------------------------------------------------------------------
 #
 #  TSDuck - The MPEG Transport Stream Toolkit
@@ -31,7 +31,13 @@
 #  This script installs all pre-requisites packages to build TSDuck,
 #  depending on the system.
 #
+#  Supported options:
+#
+#  -m, --m32 : install libraries for 32-bit cross-compilation (when supported)
+#  -s, --static : install static libraries for static build (when supported)
+#
 #  Supported operating systems:
+#
 #  - macOS
 #  - Ubuntu
 #  - Debian
@@ -48,6 +54,28 @@ SCRIPT=$(basename $0 .sh)
 SCRIPTDIR=$(cd $(dirname $0); pwd)
 error() { echo >&2 "$SCRIPT: $*"; exit 1; }
 
+# Default options.
+STATIC=false
+M32=false
+
+# Decode command line options.
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -m|--m32)
+            M32=true
+            ;;
+        -s|--static)
+            STATIC=true
+            ;;
+        *)
+            error "unsupported option $1"
+            ;;
+    esac
+    shift
+done
+
+#-----------------------------------------------------------------------------
+
 DISTRO=$(lsb_release -i 2>/dev/null | sed -e 's/.*:[\t ]*//')
 MAJOR=$(lsb_release -r 2>/dev/null | sed -e 's/.*:[\t ]*//' -e 's/\..*//')
 MINOR=$(lsb_release -r 2>/dev/null | sed -e '/\./!d' -e 's/.*:[\t ]*//' -e 's/.*\.//')
@@ -55,7 +83,7 @@ MINOR=$(lsb_release -r 2>/dev/null | sed -e '/\./!d' -e 's/.*:[\t ]*//' -e 's/.*
 if [[ $(uname -s) == Darwin ]]; then
 
     # macOS
-    pkglist="gnu-sed grep dos2unix pcsc-lite jq srt"
+    pkglist="gnu-sed grep dos2unix pcsc-lite srt"
     if [[ -z $(which clang 2>/dev/null) ]]; then
         # Build tools not installed
         xcode-select --install
@@ -74,11 +102,14 @@ if [[ $(uname -s) == Darwin ]]; then
 elif [[ "$DISTRO" == "Ubuntu" ]]; then
 
     # Ubuntu
-    pkglist="g++ dos2unix curl tar zip doxygen graphviz pcscd libpcsclite-dev dpkg-dev jq"
+    pkglist="g++ dos2unix curl tar zip doxygen graphviz pcscd libpcsclite-dev dpkg-dev"
     if [[ "$MAJOR" -le 17 ]]; then
         pkglist="$pkglist libcurl3 libcurl3-dev"
     else
         pkglist="$pkglist libcurl4 libcurl4-openssl-dev"
+    fi
+    if $M32; then
+        pkglist="$pkglist gcc-multilib"
     fi
     if [[ "$MAJOR" -ge 19 ]]; then
         pkglist="$pkglist libsrt-dev"
@@ -89,7 +120,10 @@ elif [[ "$DISTRO" == "Ubuntu" ]]; then
 elif [[ "$DISTRO" = "Debian" || "$DISTRO" = "Raspbian" ]]; then
 
     # Debian or Raspbian (Raspberry Pi)
-    pkglist="g++ dos2unix curl tar zip doxygen graphviz pcscd libpcsclite-dev dpkg-dev jq"
+    pkglist="g++ dos2unix curl tar zip doxygen graphviz pcscd libpcsclite-dev dpkg-dev"
+    if $M32; then
+        pkglist="$pkglist gcc-multilib"
+    fi
     if [[ "$MAJOR" -le 9 ]]; then
         pkglist="$pkglist libcurl3 libcurl3-dev"
     else
@@ -102,28 +136,43 @@ elif [[ -f /etc/fedora-release ]]; then
 
     # Fedora
     FC=$(grep " release " /etc/fedora-release 2>/dev/null | sed -e 's/^.* release \([0-9\.]*\) .*$/\1/')
-    pkglist="gcc-c++ dos2unix curl tar zip doxygen graphviz pcsc-tools pcsc-lite-devel libcurl libcurl-devel rpmdevtools jq"
+    pkglist="gcc-c++ dos2unix curl tar zip doxygen graphviz pcsc-tools pcsc-lite-devel libcurl libcurl-devel rpmdevtools"
+    if $STATIC; then
+        pkglist="$pkglist glibc-static libstdc++-static"
+    fi
+    if $M32; then
+        pkglist="$pkglist glibc-devel.i686 libstdc++-devel.i686 pcsc-lite-devel.i686 libcurl-devel.i686"
+    fi
     if [[ $FC -ge 31 ]]; then
         pkglist="$pkglist srt-devel"
+        if $M32; then
+            pkglist="$pkglist srt-devel.i686"
+        fi
     fi
     sudo dnf -y install $pkglist
 
 elif [[ -f /etc/redhat-release ]]; then
 
     # Red Hat or CentOS
-    pkglist="gcc-c++ dos2unix curl tar zip doxygen graphviz pcsc-lite pcsc-lite-devel libcurl libcurl-devel rpmdevtools jq"
+    pkglist="gcc-c++ dos2unix curl tar zip doxygen graphviz pcsc-lite pcsc-lite-devel libcurl libcurl-devel rpmdevtools"
+    if $STATIC; then
+        pkglist="$pkglist glibc-static libstdc++-static"
+    fi
+    if $M32; then
+        pkglist="$pkglist glibc-devel.i686 libstdc++-devel.i686 pcsc-lite-devel.i686 libcurl-devel.i686"
+    fi
     sudo yum -y install $pkglist
 
 elif [[ -f /etc/arch-release ]]; then
 
     # Arch Linux
-    pkglist="gcc dos2unix curl tar zip doxygen graphviz pcsclite jq srt"
+    pkglist="gcc dos2unix curl tar zip doxygen graphviz pcsclite srt"
     sudo pacman -Sy $pkglist
 
 elif [[ -f /etc/alpine-release ]]; then
 
     # Alpine Linux
-    pkglist="bash coreutils diffutils procps util-linux linux-headers git make g++ dos2unix curl tar zip doxygen graphviz pcsc-lite-dev curl-dev jq"
+    pkglist="bash coreutils diffutils procps util-linux linux-headers git make g++ dos2unix curl tar zip doxygen graphviz pcsc-lite-dev curl-dev"
     sudo apk add $pkglist
 
 fi
