70a68bb75ac675c1ee15b13ced7231912d05119e
Thierry Lelegard <thierry@lelegard.fr>
2020-06-07 18:59:20 +0200

    Added distro name in Debian family packages

--- build/get-version-from-sources.sh
+++ build/get-version-from-sources.sh
@@ -36,15 +36,39 @@ SCRIPT=$(basename $BASH_SOURCE)
 ROOTDIR=$(cd $(dirname $BASH_SOURCE)/..; pwd)
 VERSFILE="$ROOTDIR/src/libtsduck/tsVersion.h"
 
-TS_MAJOR=$(grep '\#define *TS_VERSION_MAJOR ' "$VERSFILE" | sed -e 's/.* //')
-TS_MINOR=$(grep '\#define *TS_VERSION_MINOR ' "$VERSFILE" | sed -e 's/.* //')
-TS_COMMIT=$(grep '\#define *TS_COMMIT ' "$VERSFILE" | sed -e 's/.* //')
-TS_VERSION=
+major()  { grep '\#define *TS_VERSION_MAJOR ' "$VERSFILE" | sed -e 's/.* //'; }
+minor()  { grep '\#define *TS_VERSION_MINOR ' "$VERSFILE" | sed -e 's/.* //'; }
+commit() { grep '\#define *TS_COMMIT ' "$VERSFILE" | sed -e 's/.* //'; }
+
+distro()
+{
+    if [[ -n "$(which lsb_release 2>/dev/null)" ]]; then
+        dis=$(lsb_release -si 2>/dev/null | tr A-Z a-z)$(lsb_release -sr 2>/dev/null | sed 's/\..*//')
+        [[ -n "$dis" ]] && dis=".$dis"
+    elif [[ -e /etc/fedora-release ]]; then
+        dis=$(grep " release " /etc/fedora-release 2>/dev/null | sed -e 's/^.* release \([0-9]*\).*$/\1/')
+        [[ -n "$dis" ]] && dis=".fc$dis"
+    elif [[ -e /etc/redhat-release ]]; then
+        dis=$(grep " release " /etc/redhat-release 2>/dev/null | sed -e 's/^.* release \([0-9]*\).*$/\1/')
+        [[ -n "$dis" ]] && dis=".el$dis"
+    elif [[ -e /etc/alpine-release ]]; then
+        dis=$(head -1 /etc/alpine-release | sed -e 's/\.[^\.]*$//' -e 's/\.//g')
+        [[ -n "$dis" ]] && dis=".alpine$dis"
+    elif [[ $(uname -s) == Darwin ]]; then
+        dis=$(sw_vers -productVersion 2>/dev/null | sed -e 's/\.[^\.]*$//' -e 's/\.//g')
+        [[ -n "$dis" ]] && dis=".macos$dis"
+    else
+        dis=
+    fi
+    echo "$dis"
+}
 
 case "$1" in
-    --major)  echo "$TS_MAJOR" ;;
-    --minor)  echo "$TS_MINOR" ;;
-    --commit) echo "$TS_COMMIT" ;;
-    --main)   echo "$TS_MAJOR.$TS_MINOR" ;;
-    *)        echo "$TS_MAJOR.$TS_MINOR-$TS_COMMIT" ;;
+    --major)  major ;;
+    --minor)  minor ;;
+    --commit) commit ;;
+    --distro) distro ;;
+    --full)   echo "$(major).$(minor)-$(commit)$(distro)" ;;
+    --main)   echo "$(major).$(minor)" ;;
+    *)        echo "$(major).$(minor)-$(commit)" ;;
 esac
