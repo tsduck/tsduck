65a51a7f437d2c485177ad406e9e44fa5181ed8c
Thierry Lelegard <thierry@lelegard.fr>
2020-06-11 22:27:53 +0200

    Updated pcrverify to work with input time-stamps

--- src/tsplugins/tsplugin_pcrverify.cpp
+++ src/tsplugins/tsplugin_pcrverify.cpp
@@ -57,17 +57,20 @@ namespace ts {
         // Description of one PID
         struct PIDContext
         {
-            PIDContext();                   // Constructor.
-            uint64_t      last_pcr_value;   // Last PCR value in this PID.
-            PacketCounter last_pcr_packet;  // Packet index containing last PCR.
+            PIDContext();                 // Constructor.
+            uint64_t      pcr_value;      // Last PCR value in this PID.
+            PacketCounter pcr_packet;     // Packet index containing last PCR.
+            uint64_t      pcr_timestamp;  // Input timestamp of packet containing last PCR (or INVALID _PCR).
         };
 
         // Command line options.
-        bool    _absolute;    // Use PCR absolute value, not micro-second
-        BitRate _bitrate;     // Expected bitrate (0 if unknown)
-        int64_t _jitter_max;  // Max jitter in PCR units
-        bool    _time_stamp;  // Display time stamps
-        PIDSet  _pid_list;    // Array of pid values to filter
+        bool    _absolute;       // Use PCR absolute value, not micro-second
+        bool    _input_synch;    // Use input-synchronous verification, base on input timestamps
+        BitRate _bitrate;        // Expected bitrate (0 if unknown)
+        int64_t _jitter_max;     // Max accepted jitter in PCR units
+        int64_t _jitter_unreal;  // Max realistic jitter
+        bool    _time_stamp;     // Display time stamps
+        PIDSet  _pid_list;       // Array of pid values to filter
 
         // Working data.
         PacketCounter            _nb_pcr_ok;         // Number of PCR without jitter
@@ -78,14 +81,9 @@ namespace ts {
         // PCR units per micro-second.
         static constexpr int64_t PCR_PER_MICRO_SEC = int64_t(SYSTEM_CLOCK_FREQ) / MicroSecPerSec;
         static constexpr int64_t DEFAULT_JITTER_MAX_US = 1000; // 1000 us = 1 ms
+        static constexpr int64_t DEFAULT_JITTER_UNREAL_US = 10 * MicroSecPerSec; // 10 seconds
         static constexpr int64_t DEFAULT_JITTER_MAX = DEFAULT_JITTER_MAX_US * PCR_PER_MICRO_SEC;
-
-        // Compute jitter. All value must be signed 64-bit.
-        int64_t jitter(int64_t pcr1,     // first PCR value
-                       int64_t pkt1,     // packet index of PCR1 in TS
-                       int64_t pcr2,     // seconde PCR value
-                       int64_t pkt2,     // packet index of PCR2 in TS
-                       int64_t bitrate); // TS bitrate in bits/sec
+        static constexpr int64_t DEFAULT_JITTER_UNREAL = DEFAULT_JITTER_UNREAL_US * PCR_PER_MICRO_SEC;
     };
 }
 
@@ -103,8 +101,9 @@ constexpr int64_t ts::PCRVerifyPlugin::DEFAULT_JITTER_MAX;
 //----------------------------------------------------------------------------
 
 ts::PCRVerifyPlugin::PIDContext::PIDContext() :
-    last_pcr_value(INVALID_PCR),
-    last_pcr_packet(0)
+    pcr_value(INVALID_PCR),
+    pcr_packet(0),
+    pcr_timestamp(INVALID_PCR)
 {
 }
 
@@ -116,8 +115,10 @@ ts::PCRVerifyPlugin::PIDContext::PIDContext() :
 ts::PCRVerifyPlugin::PCRVerifyPlugin(TSP* tsp_) :
     ProcessorPlugin(tsp_, u"Verify PCR's from TS packets", u"[options]"),
     _absolute(false),
+    _input_synch(false),
     _bitrate(0),
     _jitter_max(0),
+    _jitter_unreal(0),
     _time_stamp(false),
     _pid_list(),
     _nb_pcr_ok(0),
@@ -132,8 +133,15 @@ ts::PCRVerifyPlugin::PCRVerifyPlugin(TSP* tsp_) :
 
     option(u"bitrate", 'b', POSITIVE);
     help(u"bitrate",
-         u"Verify the PCR's according to this transport bitrate. By default, "
-         u"use the input bitrate as reported by the input device.");
+         u"Verify the PCR's according to this transport bitrate. "
+         u"By default, use the input bitrate as reported by the input device.");
+
+    option(u"input-synchronous", 'i');
+    help(u"input-synchronous",
+         u"Verify the PCR's according to each packet input timestamp. "
+         u"This method is meaningful only with real-time input sources or if the input "
+         u"source can recreate reliable input timestamps (M2TS files for instance). "
+         u"With this option, the bitrate is ignored.");
 
     option(u"jitter-max", 'j', UNSIGNED);
     help(u"jitter-max",
@@ -142,6 +150,15 @@ ts::PCRVerifyPlugin::PCRVerifyPlugin(TSP* tsp_) :
          u"otherwise it is in micro-seconds. The default is " +
          UString::Decimal(DEFAULT_JITTER_MAX) + u" PCR units or " +
          UString::Decimal(DEFAULT_JITTER_MAX_US) + u" micro-seconds.");
+
+    option(u"jitter-unreal", 0, UNSIGNED);
+    help(u"jitter-unreal",
+         u"Maximum realistic jitter. Any jitter above this value is unrealistic and ignored "
+         u"(probably because of a PCR leap). If --absolute, the specified value is in PCR units, "
+         u"otherwise it is in micro-seconds. The default is " +
+         UString::Decimal(DEFAULT_JITTER_UNREAL) + u" PCR units or " +
+         UString::Decimal(DEFAULT_JITTER_UNREAL_US) + u" micro-seconds (" +
+         UString::Decimal(DEFAULT_JITTER_UNREAL_US / MicroSecPerSec) + u" seconds).");
 
     option(u"pid", 'p', PIDVAL, 0, UNLIMITED_COUNT);
     help(u"pid", u"pid1[-pid2]",
@@ -161,7 +178,9 @@ ts::PCRVerifyPlugin::PCRVerifyPlugin(TSP* tsp_) :
 bool ts::PCRVerifyPlugin::getOptions()
 {
     _absolute = present(u"absolute");
+    _input_synch = present(u"input-synchronous");
     _jitter_max = intValue<int64_t>(u"jitter-max", _absolute ? DEFAULT_JITTER_MAX : DEFAULT_JITTER_MAX_US);
+    _jitter_unreal = intValue<int64_t>(u"jitter-unreal", _absolute ? DEFAULT_JITTER_UNREAL : DEFAULT_JITTER_UNREAL_US);
     _bitrate = intValue<BitRate>(u"bitrate", 0);
     _time_stamp = present(u"time-stamp");
     getIntValues(_pid_list, u"pid", true); // all PID's set by default
@@ -169,7 +188,13 @@ bool ts::PCRVerifyPlugin::getOptions()
     if (!_absolute) {
         // Convert _jitter_max from micro-second to PCR units
         _jitter_max *= PCR_PER_MICRO_SEC;
+        _jitter_unreal *= PCR_PER_MICRO_SEC;
     }
+    if (_bitrate > 0 && _input_synch) {
+        tsp->error(u"options --bitrate and --input-synchronous are mutually exclusive");
+        return false;
+    }
+
     return true;
 }
 
@@ -202,37 +227,6 @@ bool ts::PCRVerifyPlugin::stop()
 
 
 //----------------------------------------------------------------------------
-// Compute the PCR jitter
-//----------------------------------------------------------------------------
-
-int64_t ts::PCRVerifyPlugin::jitter(int64_t pcr1, int64_t pkt1, int64_t pcr2, int64_t pkt2, int64_t bitrate)
-{
-    // Cannot compute jitter if bitrate is unknown
-    if (bitrate == 0) {
-        return 0;
-    }
-
-    // Adjust second PCR if PCR's have looped back after max value.
-    // Since a PCR is coded on 42 bits, adjusting the value remains within 64 bits.
-    if (ts::WrapUpPCR(pcr1, pcr2)) {
-        pcr2 += ts::PCR_SCALE;
-    }
-
-    // sysclock = 27 MHz = 27,000,000 = SYSTEM_CLOCK_FREQ
-    // seconds = bits / bitrate
-    // bits = (pkt2 - pkt1) * PKT_SIZE * 8
-    // epcr2 = expected pcr2 based on bitrate
-    //       = pcr1 + (seconds * sysclock)
-    //       = pcr1 + (bits * sysclock) / bitrate
-    //       = pcr1 + ((pkt2 - pkt1) * PKT_SIZE * 8 * sysclock) / bitrate
-    const int64_t epcr2 = pcr1 + ((pkt2 - pkt1) * PKT_SIZE * 8 * SYSTEM_CLOCK_FREQ) / bitrate;
-
-    // Jitter = difference between actual and expected pcr2
-    return pcr2 - epcr2;
-}
-
-
-//----------------------------------------------------------------------------
 // Packet processing method
 //----------------------------------------------------------------------------
 
@@ -243,26 +237,71 @@ ts::ProcessorPlugin::Status ts::PCRVerifyPlugin::processPacket(TSPacket& pkt, TS
     // Check if this PID shall be filtered and packet has a PCR
     if (_pid_list[pid] && pkt.hasPCR()) {
 
-        const uint64_t pcr = pkt.getPCR();
+        // PID context at previous PCR packet.
         PIDContext& pc(_stats[pid]);
 
-        // Compare PCR with previous one (if there is one)
-        if (pc.last_pcr_value == INVALID_PCR) {
+        // PID context at current packet.
+        PIDContext next_pc;
+        next_pc.pcr_value = pkt.getPCR();
+        next_pc.pcr_packet = tsp->pluginPackets();
+        next_pc.pcr_timestamp = pkt_data.getInputTimeStamp(); // in PCR units, INVALID_PCR if there is no timestamp
+
+        // Current bitrate is needed if not --input-synchronous. Use signed 64-bit for jitter computation.
+        const int64_t bitrate = int64_t(_bitrate != 0 || _input_synch ? _bitrate : tsp->bitrate());
+
+        // Compare this PCR with previous one to compute the jitter.
+        if (pc.pcr_value == INVALID_PCR) {
+            // First PCR in the PID, no previous value to compare with.
+            _nb_pcr_unchecked++;
+        }
+        else if (_input_synch && (pc.pcr_timestamp == INVALID_PCR || next_pc.pcr_timestamp == INVALID_PCR)) {
+            // Use input timestamps to compute jitter but at least one of them is unknown.
+            _nb_pcr_unchecked++;
+        }
+        else if (!_input_synch && bitrate == 0) {
+            // Use bitrate to compute jitter but bitrate is unknown.
             _nb_pcr_unchecked++;
         }
         else {
-            // Current bitrate:
-            const int64_t bitrate = int64_t(_bitrate != 0 ? _bitrate : tsp->bitrate());
-            // PCR jitter:
-            const int64_t jit = jitter(int64_t(pc.last_pcr_value),
-                                       int64_t(pc.last_pcr_packet),
-                                       int64_t(pcr),
-                                       int64_t(tsp->pluginPackets()),
-                                       bitrate);
+            // To compute jitter, all values must be signed 64-bit.
+            int64_t jitter = 0;
+            int64_t pcr1 = int64_t(pc.pcr_value);
+            int64_t pcr2 = int64_t(next_pc.pcr_value);
+
+            // Adjust second PCR if PCR's have looped back after max value.
+            // Since a PCR is coded on 42 bits, adjusting the value remains within 64 bits.
+            if (ts::WrapUpPCR(pcr1, pcr2)) {
+                pcr2 += ts::PCR_SCALE;
+            }
+
+            if (_input_synch) {
+                // Compute jitter based on input timestamps (they are in PCR units).
+                const int64_t pcr_diff = pcr2 - pcr1;
+                const int64_t time_diff = int64_t(next_pc.pcr_timestamp) - int64_t(pc.pcr_timestamp);
+                // Jitter = difference between actual and expected duration.
+                jitter = time_diff - pcr_diff;
+            }
+            else {
+                // Compute jitter based on "locally stable" bitrate.
+                // sysclock = 27 MHz = 27,000,000 = SYSTEM_CLOCK_FREQ
+                // seconds = bits / bitrate
+                // bits = (pkt2 - pkt1) * PKT_SIZE * 8
+                // epcr2 = expected pcr2 based on bitrate
+                //       = pcr1 + (seconds * sysclock)
+                //       = pcr1 + (bits * sysclock) / bitrate
+                //       = pcr1 + ((pkt2 - pkt1) * PKT_SIZE * 8 * sysclock) / bitrate
+                const int64_t epcr2 = pcr1 + (int64_t(next_pc.pcr_packet - pc.pcr_packet) * PKT_SIZE * 8 * SYSTEM_CLOCK_FREQ) / bitrate;
+                // Jitter = difference between actual and expected pcr2
+                jitter = pcr2 - epcr2;
+            }
+
             // Absolute value of PCR jitter:
-            const int64_t ajit = jit >= 0 ? jit : -jit;
+            const int64_t ajit = jitter >= 0 ? jitter : -jitter;
             if (ajit <= _jitter_max) {
                 _nb_pcr_ok++;
+            }
+            else if (ajit > _jitter_unreal) {
+                _nb_pcr_unchecked++;
             }
             else {
                 _nb_pcr_nok++;
@@ -270,13 +309,12 @@ ts::ProcessorPlugin::Status ts::PCRVerifyPlugin::processPacket(TSPacket& pkt, TS
                 const int64_t bit_jit = (ajit * bitrate) / SYSTEM_CLOCK_FREQ;
                 tsp->info(u"%sPID %d (0x%X), PCR jitter: %'d = %'d micro-seconds = %'d packets + %'d bytes + %'d bits",
                           {_time_stamp ? (Time::CurrentLocalTime().format(Time::DATE | Time::TIME) + u", ") : u"",
-                           pid, pid, jit, ajit / PCR_PER_MICRO_SEC, bit_jit / (PKT_SIZE * 8), (bit_jit / 8) % PKT_SIZE, bit_jit % 8});
+                           pid, pid, jitter, ajit / PCR_PER_MICRO_SEC, bit_jit / (PKT_SIZE * 8), (bit_jit / 8) % PKT_SIZE, bit_jit % 8});
             }
         }
 
         // Remember PCR position
-        pc.last_pcr_value = pcr;
-        pc.last_pcr_packet = tsp->pluginPackets();
+        pc = next_pc;
     }
 
     return TSP_OK;
