e82bd58a19927aa855a66527266bc6ee55812b65
Thierry Lelegard <thierry@lelegard.fr>
2020-09-03 18:02:34 +0200

    Get rid of annoying GCC message on ABI compatibility on ARM

--- src/libtsduck/tsVersion.h
+++ src/libtsduck/tsVersion.h
@@ -40,8 +40,8 @@
 //!
 //! TSDuck minor version.
 //!
-#define TS_VERSION_MINOR 23
+#define TS_VERSION_MINOR 24
 //!
 //! TSDuck commit number (automatically updated by Git hooks).
 //!
-#define TS_COMMIT 1965
+#define TS_COMMIT 1967
