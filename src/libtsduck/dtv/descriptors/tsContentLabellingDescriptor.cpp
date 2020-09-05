584689a6ebebd266f4ada50595512d861652162e
Thierry Lelegard <thierry@lelegard.fr>
2020-09-06 00:26:35 +0200

    Added descriptors

---
+++ src/libtsduck/dtv/descriptors/tsContentLabellingDescriptor.cpp
@@ -0,0 +1,237 @@
+//----------------------------------------------------------------------------
+//
+// TSDuck - The MPEG Transport Stream Toolkit
+// Copyright (c) 2005-2020, Thierry Lelegard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+//----------------------------------------------------------------------------
+
+#include "tsContentLabellingDescriptor.h"
+#include "tsDescriptor.h"
+#include "tsTablesDisplay.h"
+#include "tsPSIRepository.h"
+#include "tsPSIBuffer.h"
+#include "tsDuckContext.h"
+#include "tsxmlElement.h"
+#include "tsNames.h"
+TSDUCK_SOURCE;
+
+#define MY_XML_NAME u"content_labelling_descriptor"
+#define MY_CLASS ts::ContentLabellingDescriptor
+#define MY_DID ts::DID_CONTENT_LABELLING
+#define MY_STD ts::Standards::MPEG
+
+TS_REGISTER_DESCRIPTOR(MY_CLASS, ts::EDID::Standard(MY_DID), MY_XML_NAME, MY_CLASS::DisplayDescriptor);
+
+
+//----------------------------------------------------------------------------
+// Constructors
+//----------------------------------------------------------------------------
+
+ts::ContentLabellingDescriptor::ContentLabellingDescriptor() :
+    AbstractDescriptor(MY_DID, MY_XML_NAME, MY_STD, 0),
+    metadata_application_format(0),
+    metadata_application_format_identifier(0),
+    content_time_base_indicator(0),
+    content_reference_id(),
+    content_time_base_value(0),
+    metadata_time_base_value(0),
+    content_id(0),
+    time_base_association_data(),
+    private_data()
+{
+}
+
+void ts::ContentLabellingDescriptor::clearContent()
+{
+    metadata_application_format = 0;
+    metadata_application_format_identifier = 0;
+    content_time_base_indicator = 0;
+    content_reference_id.clear();
+    content_time_base_value = 0;
+    metadata_time_base_value = 0;
+    content_id = 0;
+    time_base_association_data.clear();
+    private_data.clear();
+}
+
+ts::ContentLabellingDescriptor::ContentLabellingDescriptor(DuckContext& duck, const Descriptor& desc) :
+    ContentLabellingDescriptor()
+{
+    deserialize(duck, desc);
+}
+
+
+//----------------------------------------------------------------------------
+// Serialization.
+//----------------------------------------------------------------------------
+
+void ts::ContentLabellingDescriptor::serializePayload(PSIBuffer& buf) const
+{
+    buf.putUInt16(metadata_application_format);
+    if (metadata_application_format == 0xFFFF) {
+        buf.putUInt32(metadata_application_format_identifier);
+    }
+    buf.putBit(!content_reference_id.empty());
+    buf.putBits(content_time_base_indicator, 4);
+    buf.putBits(0xFF, 3);
+    if (!content_reference_id.empty()) {
+        buf.putUInt8(uint8_t(content_reference_id.size()));
+        buf.putBytes(content_reference_id);
+    }
+    if (content_time_base_indicator == 1 || content_time_base_indicator == 2) {
+        buf.putBits(0xFF, 7);
+        buf.putBits(content_time_base_value, 33);
+        buf.putBits(0xFF, 7);
+        buf.putBits(metadata_time_base_value, 33);
+    }
+    if (content_time_base_indicator == 2) {
+        buf.putBit(1);
+        buf.putBits(content_id, 7);
+    }
+    if (content_time_base_indicator >= 3 && content_time_base_indicator <= 7) {
+        buf.putUInt8(uint8_t(time_base_association_data.size()));
+        buf.putBytes(time_base_association_data);
+    }
+    buf.putBytes(private_data);
+}
+
+
+//----------------------------------------------------------------------------
+// Deserialization.
+//----------------------------------------------------------------------------
+
+void ts::ContentLabellingDescriptor::deserializePayload(PSIBuffer& buf)
+{
+    metadata_application_format = buf.getUInt16();
+    if (metadata_application_format == 0xFFFF) {
+        metadata_application_format_identifier = buf.getUInt32();
+    }
+    const bool content_reference_id_record_flag = buf.getBit() != 0;
+    content_time_base_indicator = buf.getBits<uint8_t>(4);
+    buf.skipBits(3);
+    if (content_reference_id_record_flag) {
+        const size_t length = buf.getUInt8();
+        buf.getByteBlock(content_reference_id, length);
+    }
+    if (content_time_base_indicator == 1 || content_time_base_indicator == 2) {
+        buf.skipBits(7);
+        content_time_base_value = buf.getBits<uint64_t>(33);
+        buf.skipBits(7);
+        metadata_time_base_value = buf.getBits<uint64_t>(33);
+    }
+    if (content_time_base_indicator == 2) {
+        buf.skipBits(1);
+        content_id = buf.getBits<uint8_t>(7);
+    }
+    if (content_time_base_indicator >= 3 && content_time_base_indicator <= 7) {
+        const size_t length = buf.getUInt8();
+        buf.getByteBlock(time_base_association_data, length);
+    }
+    buf.getByteBlock(private_data, buf.remainingReadBytes());
+}
+
+
+//----------------------------------------------------------------------------
+// Static method to display a descriptor.
+//----------------------------------------------------------------------------
+
+void ts::ContentLabellingDescriptor::DisplayDescriptor(TablesDisplay& disp, PSIBuffer& buf, const UString& margin, DID did, TID tid, PDS pds)
+{
+    if (buf.remainingReadBytes() >= 2) {
+        const uint16_t format = buf.getUInt16();
+        disp << margin << "Metadata application format: " << NameFromSection(u"MetadataApplicationFormat", format, names::HEXA_FIRST) << std::endl;
+        if (format == 0xFFFF && buf.remainingReadBytes() >= 4) {
+            disp << margin << UString::Format(u"Metadata application format identifier: 0x%X (%<d)", {buf.getUInt32()}) << std::endl;
+        }
+        const bool content_reference_id_record_flag = buf.getBit() != 0;
+        const uint8_t time_base_indicator = buf.getBits<uint8_t>(4);
+        disp << margin << "Content time base indicator: " << NameFromSection(u"ContentTimeBaseIndicator", time_base_indicator, names::HEXA_FIRST) << std::endl;
+        buf.skipBits(3);
+        if (content_reference_id_record_flag && buf.remainingReadBytes() >= 1) {
+            const size_t length = buf.getUInt8();
+            disp.displayPrivateData(u"Content reference id", buf, length, margin);
+        }
+        if (time_base_indicator == 1 || time_base_indicator == 2) {
+            buf.skipBits(7);
+            disp << margin << UString::Format(u"Content time base: 0x%09X (%<'d)", {buf.getBits<uint64_t>(33)}) << std::endl;
+            buf.skipBits(7);
+            disp << margin << UString::Format(u"Metadata time base: 0x%09X (%<'d)", {buf.getBits<uint64_t>(33)}) << std::endl;
+        }
+        if (time_base_indicator == 2) {
+            buf.skipBits(1);
+            disp << margin << UString::Format(u"Content id: 0x%X (%<d)", {buf.getBits<uint8_t>(7)}) << std::endl;
+        }
+        if (time_base_indicator >= 3 && time_base_indicator <= 7) {
+            const size_t length = buf.getUInt8();
+            disp.displayPrivateData(u"Time base association data", buf, length, margin);
+        }
+        disp.displayPrivateData(u"Private data", buf, buf.remainingReadBytes(), margin);
+    }
+    disp.displayExtraData(buf, margin);
+}
+
+
+//----------------------------------------------------------------------------
+// XML serialization.
+//----------------------------------------------------------------------------
+
+void ts::ContentLabellingDescriptor::buildXML(DuckContext& duck, xml::Element* root) const
+{
+    root->setIntAttribute(u"metadata_application_format", metadata_application_format, true);
+    if (metadata_application_format == 0xFFFF) {
+        root->setIntAttribute(u"metadata_application_format_identifier", metadata_application_format_identifier, true);
+    }
+    root->setIntAttribute(u"content_time_base_indicator", content_time_base_indicator, false);
+    root->addHexaTextChild(u"content_reference_id", content_reference_id, true);
+    if (content_time_base_indicator == 1 || content_time_base_indicator == 2) {
+        root->setIntAttribute(u"content_time_base_value", content_time_base_value, false);
+        root->setIntAttribute(u"metadata_time_base_value", metadata_time_base_value, false);
+    }
+    if (content_time_base_indicator == 2) {
+        root->setIntAttribute(u"content_id", content_id, true);
+    }
+    if (content_time_base_indicator >= 3 && content_time_base_indicator <= 7) {
+        root->addHexaTextChild(u"time_base_association_data", time_base_association_data, true);
+    }
+    root->addHexaTextChild(u"private_data", private_data, true);
+}
+
+
+//----------------------------------------------------------------------------
+// XML deserialization.
+//----------------------------------------------------------------------------
+
+bool ts::ContentLabellingDescriptor::analyzeXML(DuckContext& duck, const xml::Element* element)
+{
+    return element->getIntAttribute<uint16_t>(metadata_application_format, u"metadata_application_format", true) &&
+           element->getIntAttribute<uint32_t>(metadata_application_format_identifier, u"metadata_application_format_identifier", metadata_application_format == 0xFFFF) &&
+           element->getIntAttribute<uint8_t>(content_time_base_indicator, u"content_time_base_indicator", true, 0, 0, 15) &&
+           element->getHexaTextChild(content_reference_id, u"content_reference_id", false, 0, 255) &&
+           element->getIntAttribute<uint64_t>(content_time_base_value, u"content_time_base_value", content_time_base_indicator == 1 || content_time_base_indicator == 2, 0, 0, TS_UCONST64(0x1FFFFFFFF)) &&
+           element->getIntAttribute<uint64_t>(metadata_time_base_value, u"metadata_time_base_value", content_time_base_indicator == 1 || content_time_base_indicator == 2, 0, 0, TS_UCONST64(0x1FFFFFFFF)) &&
+           element->getIntAttribute<uint8_t>(content_id, u"content_id", content_time_base_indicator == 2, 0, 0, 0x7F) &&
+           element->getHexaTextChild(time_base_association_data, u"time_base_association_data", false, 0, 255) &&
+           element->getHexaTextChild(private_data, u"private_data", false, 0, 255);
+}
---
+++ src/libtsduck/dtv/descriptors/tsMetadataPointerDescriptor.cpp
@@ -0,0 +1,255 @@
+//----------------------------------------------------------------------------
+//
+// TSDuck - The MPEG Transport Stream Toolkit
+// Copyright (c) 2005-2020, Thierry Lelegard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+//----------------------------------------------------------------------------
+
+#include "tsMetadataPointerDescriptor.h"
+#include "tsDescriptor.h"
+#include "tsTablesDisplay.h"
+#include "tsPSIRepository.h"
+#include "tsPSIBuffer.h"
+#include "tsDuckContext.h"
+#include "tsxmlElement.h"
+#include "tsNames.h"
+TSDUCK_SOURCE;
+
+#define MY_XML_NAME u"metadata_pointer_descriptor"
+#define MY_CLASS ts::MetadataPointerDescriptor
+#define MY_DID ts::DID_METADATA_POINTER
+#define MY_STD ts::Standards::MPEG
+
+TS_REGISTER_DESCRIPTOR(MY_CLASS, ts::EDID::Standard(MY_DID), MY_XML_NAME, MY_CLASS::DisplayDescriptor);
+
+
+//----------------------------------------------------------------------------
+// Constructors
+//----------------------------------------------------------------------------
+
+ts::MetadataPointerDescriptor::MetadataPointerDescriptor() :
+    AbstractDescriptor(MY_DID, MY_XML_NAME, MY_STD, 0),
+    metadata_application_format(0),
+    metadata_application_format_identifier(0),
+    metadata_format(0),
+    metadata_format_identifier(0),
+    metadata_service_id(0),
+    MPEG_carriage_flags(0),
+    metadata_locator(),
+    program_number(0),
+    transport_stream_location(0),
+    transport_stream_id(0),
+    private_data()
+{
+}
+
+void ts::MetadataPointerDescriptor::clearContent()
+{
+    metadata_application_format = 0;
+    metadata_application_format_identifier = 0;
+    metadata_format = 0;
+    metadata_format_identifier = 0;
+    metadata_service_id = 0;
+    MPEG_carriage_flags = 0;
+    metadata_locator.clear();
+    program_number = 0;
+    transport_stream_location = 0;
+    transport_stream_id = 0;
+    private_data.clear();
+}
+
+ts::MetadataPointerDescriptor::MetadataPointerDescriptor(DuckContext& duck, const Descriptor& desc) :
+    MetadataPointerDescriptor()
+{
+    deserialize(duck, desc);
+}
+
+
+//----------------------------------------------------------------------------
+// Serialization.
+//----------------------------------------------------------------------------
+
+void ts::MetadataPointerDescriptor::serializePayload(PSIBuffer& buf) const
+{
+    buf.putUInt16(metadata_application_format);
+    if (metadata_application_format == 0xFFFF) {
+        buf.putUInt32(metadata_application_format_identifier);
+    }
+    buf.putUInt8(metadata_format);
+    if (metadata_format == 0xFF) {
+        buf.putUInt32(metadata_format_identifier);
+    }
+    buf.putUInt8(metadata_service_id);
+    buf.putBit(!metadata_locator.empty());
+    buf.putBits(MPEG_carriage_flags, 2);
+    buf.putBits(0xFF, 5);
+    if (!metadata_locator.empty()) {
+        buf.putUInt8(uint8_t(metadata_locator.size()));
+        buf.putBytes(metadata_locator);
+    }
+    if (MPEG_carriage_flags <= 2) {
+        buf.putUInt16(program_number);
+    }
+    if (MPEG_carriage_flags == 1) {
+        buf.putUInt16(transport_stream_location);
+        buf.putUInt16(transport_stream_id);
+    }
+    buf.putBytes(private_data);
+}
+
+
+//----------------------------------------------------------------------------
+// Deserialization.
+//----------------------------------------------------------------------------
+
+void ts::MetadataPointerDescriptor::deserializePayload(PSIBuffer& buf)
+{
+    metadata_application_format = buf.getUInt16();
+    if (metadata_application_format == 0xFFFF) {
+        metadata_application_format_identifier = buf.getUInt32();
+    }
+    metadata_format = buf.getUInt8();
+    if (metadata_format == 0xFF) {
+        metadata_format_identifier = buf.getUInt32();
+    }
+    metadata_service_id = buf.getUInt8();
+    const bool metadata_locator_record_flag = buf.getBit() != 0;
+    MPEG_carriage_flags = buf.getBits<uint8_t>(2);
+    buf.skipBits(5);
+    if (metadata_locator_record_flag) {
+        const size_t length = buf.getUInt8();
+        buf.getByteBlock(metadata_locator, length);
+    }
+    if (MPEG_carriage_flags <= 2) {
+        program_number = buf.getUInt16();
+    }
+    if (MPEG_carriage_flags == 1) {
+        transport_stream_location = buf.getUInt16();
+        transport_stream_id = buf.getUInt16();
+    }
+    buf.getByteBlock(private_data, buf.remainingReadBytes());
+}
+
+
+//----------------------------------------------------------------------------
+// Static method to display a descriptor.
+//----------------------------------------------------------------------------
+
+void ts::MetadataPointerDescriptor::DisplayDescriptor(TablesDisplay& disp, PSIBuffer& buf, const UString& margin, DID did, TID tid, PDS pds)
+{
+    if (buf.remainingReadBytes() < 2) {
+        buf.setUserError();
+    }
+    else {
+        const uint16_t format = buf.getUInt16();
+        disp << margin << "Metadata application format: " << NameFromSection(u"MetadataApplicationFormat", format, names::HEXA_FIRST) << std::endl;
+        if (format == 0xFFFF && buf.remainingReadBytes() >= 4) {
+            disp << margin << UString::Format(u"Metadata application format identifier: 0x%X (%<d)", {buf.getUInt32()}) << std::endl;
+        }
+    }
+
+    if (buf.remainingReadBytes() < 1) {
+        buf.setUserError();
+    }
+    else {
+        const uint8_t format = buf.getUInt8();
+        disp << margin << "Metadata format: " << NameFromSection(u"MetadataFormat", format, names::HEXA_FIRST) << std::endl;
+        if (format == 0xFF && buf.remainingReadBytes() >= 4) {
+            disp << margin << UString::Format(u"Metadata format identifier: 0x%X (%<d)", {buf.getUInt32()}) << std::endl;
+        }
+    }
+
+    if (buf.remainingReadBytes() < 2) {
+        buf.setUserError();
+    }
+    else {
+        disp << margin << UString::Format(u"Metadata service id: 0x%X (%<d)", {buf.getUInt8()}) << std::endl;
+        const bool metadata_locator_record_flag = buf.getBit() != 0;
+        const uint8_t flags = buf.getBits<uint8_t>(2);
+        disp << margin << "MPEG carriage flags: " << NameFromSection(u"MetadataCarriageFlags", flags, names::DECIMAL_FIRST) << std::endl;
+        buf.skipBits(5);
+        if (metadata_locator_record_flag) {
+            const size_t length = buf.getUInt8();
+            disp.displayPrivateData(u"Metadata location record", buf, length, margin);
+        }
+        if (flags <= 2 && buf.remainingReadBytes() >= 2) {
+            disp << margin << UString::Format(u"Program number: 0x%X (%<d)", {buf.getUInt16()}) << std::endl;
+        }
+        if (flags == 1 && buf.remainingReadBytes() >= 4) {
+            disp << margin << UString::Format(u"Transport stream location: 0x%X (%<d)", {buf.getUInt16()}) << std::endl;
+            disp << margin << UString::Format(u"Transport stream id: 0x%X (%<d)", {buf.getUInt16()}) << std::endl;
+        }
+        disp.displayPrivateData(u"Private data", buf, buf.remainingReadBytes(), margin);
+    }
+    disp.displayExtraData(buf, margin);
+}
+
+
+//----------------------------------------------------------------------------
+// XML serialization.
+//----------------------------------------------------------------------------
+
+void ts::MetadataPointerDescriptor::buildXML(DuckContext& duck, xml::Element* root) const
+{
+    root->setIntAttribute(u"metadata_application_format", metadata_application_format, true);
+    if (metadata_application_format == 0xFFFF) {
+        root->setIntAttribute(u"metadata_application_format_identifier", metadata_application_format_identifier, true);
+    }
+    root->setIntAttribute(u"metadata_format", metadata_format, true);
+    if (metadata_format == 0xFF) {
+        root->setIntAttribute(u"metadata_format_identifier", metadata_format_identifier, true);
+    }
+    root->setIntAttribute(u"metadata_service_id", metadata_service_id, true);
+    root->setIntAttribute(u"MPEG_carriage_flags", MPEG_carriage_flags, false);
+    root->addHexaTextChild(u"metadata_locator", metadata_locator, true);
+    if (MPEG_carriage_flags <= 2) {
+        root->setIntAttribute(u"program_number", program_number, true);
+    }
+    if (MPEG_carriage_flags == 1) {
+        root->setIntAttribute(u"transport_stream_location", transport_stream_location, true);
+        root->setIntAttribute(u"transport_stream_id", transport_stream_id, true);
+    }
+    root->addHexaTextChild(u"private_data", private_data, true);
+}
+
+
+//----------------------------------------------------------------------------
+// XML deserialization.
+//----------------------------------------------------------------------------
+
+bool ts::MetadataPointerDescriptor::analyzeXML(DuckContext& duck, const xml::Element* element)
+{
+    return element->getIntAttribute<uint16_t>(metadata_application_format, u"metadata_application_format", true) &&
+           element->getIntAttribute<uint32_t>(metadata_application_format_identifier, u"metadata_application_format_identifier", metadata_application_format == 0xFFFF) &&
+           element->getIntAttribute<uint8_t>(metadata_format, u"metadata_format", true) &&
+           element->getIntAttribute<uint32_t>(metadata_format_identifier, u"metadata_format_identifier", metadata_format == 0xFF) &&
+           element->getIntAttribute<uint8_t>(metadata_service_id, u"metadata_service_id", true) &&
+           element->getIntAttribute<uint8_t>(MPEG_carriage_flags, u"MPEG_carriage_flags", true, 0, 0, 3) &&
+           element->getHexaTextChild(metadata_locator, u"metadata_locator", false, 0, 255) &&
+           element->getIntAttribute<uint16_t>(program_number, u"program_number", MPEG_carriage_flags <= 2) &&
+           element->getIntAttribute<uint16_t>(transport_stream_location, u"transport_stream_location", MPEG_carriage_flags == 1) &&
+           element->getIntAttribute<uint16_t>(transport_stream_id, u"transport_stream_id", MPEG_carriage_flags == 1) &&
+           element->getHexaTextChild(private_data, u"private_data", false, 0, 255);
+}
