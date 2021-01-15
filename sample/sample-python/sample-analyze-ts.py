28517e8e5b258d61382002da5a60bb9dd7aa7ace
Thierry Lelegard <thierry@lelegard.fr>
2021-01-16 00:24:31 +0100

    Added Python sample to analyze a TS

---
+++ sample/sample-python/sample-analyze-ts.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#----------------------------------------------------------------------------
+#
+# TSDuck sample Python application : analyze a transport stream.
+# A TSProcessor is used, using plugin "analyze" and JSON output.
+# The JSON output is intercepted and parsed in a Python class.
+# When found, the list of services in the transport stream is displayed.
+# Any other information on the TS can be similarly extracted and used.
+#
+#----------------------------------------------------------------------------
+
+import ts
+import json
+
+# This string is a user-defined marker to locate the JSON line in the log.
+# It can be anything that is sufficiently weird to be unique in the logs.
+json_marker = "@@_JSON_HERE_@@"
+
+# This method processes the parsed JSON data from the TS analysis.
+# Here, we just display the list of services.
+def process_json(data):
+    for srv in data['services']:
+        print('Name: "%s", provider: "%s", bitrate: %d b/s' % (srv['name'], srv['provider'], srv['bitrate']))
+
+# A Python class which handles TSDuck log messages.
+class Logger(ts.AbstractAsyncReport):
+    # This method is invoked each time a message is logged by TSDuck.
+    def log(self, severity, message):
+        # Filter, locate, extract and parse the JSON output from plugin "analyze".
+        pos = message.find(json_marker)
+        if pos >= 0:
+            process_json(json.loads(message[pos+len(json_marker):]))
+
+# Main program: see details in "sample-tsp.py" or "sample-message-handling.py".
+rep = Logger()
+tsp = ts.TSProcessor(rep)
+
+tsp.input = ['file', 'file.ts']
+tsp.plugins = [ ['analyze', '--json-line=' + json_marker] ]
+tsp.output = ['drop']
+
+tsp.start()
+tsp.waitForTermination()
+tsp.delete()
+
+rep.terminate()
+rep.delete()
