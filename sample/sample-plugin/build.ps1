1b71ce82aa288c7f02b4e6ba21707020d96c697c
Thierry Lelegard <thierry@lelegard.fr>
2021-03-25 00:21:01 +0100

    Fixed old sample build script

--- sample/sample-plugin/build.ps1
+++ sample/sample-plugin/build.ps1
@@ -3,17 +3,19 @@
 # Directory of this script.
 $dir = $PSScriptRoot
 
-# Add various locations for MSBuild.exe, depending on your version of Visual Studio
-$env:Path = $env:Path + `
-    ';C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64' + `
-    ';C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin' + `
-    ';C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64' + `
-    ';C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin' + `
-    ';C:\Program Files (x86)\MSBuild\14.0\Bin\amd64' + `
-    ';C:\Program Files (x86)\MSBuild\14.0\Bin'
+# Find MSBuild.exe.
+$MSBuild = Get-ChildItem -Recurse -Path "C:\Program Files*\Microsoft Visual Studio" -Include MSBuild.exe -ErrorAction Ignore |
+           ForEach-Object { (Get-Command $_).FileVersionInfo } |
+           Sort-Object -Unique -Property FileVersion |
+           ForEach-Object { $_.FileName} |
+           Select-Object -Last 1
+if (-not $MSBuild) {
+    Write-Host "ERROR: MSBuild not found"
+    exit
+}
 
 # Build the project for Release x64.
-MSBuild $dir\sample-plugin.sln /nologo /maxcpucount /property:Configuration=Release /property:Platform=x64
+& $MSBuild $dir\sample-plugin.sln /nologo /maxcpucount /property:Configuration=Release /property:Platform=x64
 
 # Copy the plugin DLL to the project directory.
 # Not always a good idea, this is just for test.
