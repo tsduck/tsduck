9ee210b39f4b2e7ed4320fcf0743dab0ee095581
Thierry Lelegard <thierry@lelegard.fr>
2021-08-09 22:40:44 +0200

    Bug fix on Linux using ISDB-S

--- src/libtsduck/dtv/linux/tsTunerDevice.cpp
+++ src/libtsduck/dtv/linux/tsTunerDevice.cpp
@@ -755,10 +755,10 @@ bool ts::TunerDevice::getCurrentTuning(ModulationArgs& params, bool reset_unknow
         case DS_ISDB_S: {
             // Note: same remark about the frequency as DVB-S tuner.
             if (reset_unknown) {
-                params.frequency = 0;
-                params.polarity = ModulationArgs::DEFAULT_POLARITY;
-                params.satellite_number = ModulationArgs::DEFAULT_SATELLITE_NUMBER;
+                params.frequency.clear();
+                params.satellite_number.clear();
                 params.lnb.clear();
+                params.polarity.clear();
             }
 
             props.clear();
