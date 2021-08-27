da98d9a96066cbe74006938719af3efbd1cd83ca
wongfei2009 <wongfei2009@gmail.com>
2021-08-27 15:34:44 +0800

    Support monitoring preroll time for time signal command

--- src/libtsduck/dtv/tables/tsSpliceInformationTable.cpp
+++ src/libtsduck/dtv/tables/tsSpliceInformationTable.cpp
@@ -127,9 +127,19 @@ void ts::SpliceInformationTable::clearContent()
 
 void ts::SpliceInformationTable::adjustPTS()
 {
-    // Only splice_insert() commands need adjustment.
+    // Ignore null or invalid adjustment.
+    if (pts_adjustment == 0 || pts_adjustment > PTS_DTS_MASK) {
+        return;
+    }
+
+    // Only splice_insert() and time_signal() commands need adjustment.
     if (splice_command_type == SPLICE_INSERT) {
         splice_insert.adjustPTS(pts_adjustment);
+    } else if (splice_command_type == SPLICE_TIME_SIGNAL) {
+        // Adjust time signal time.
+        if (time_signal.set() && time_signal.value() <= PTS_DTS_MASK) {
+            time_signal = (time_signal.value() + pts_adjustment) & PTS_DTS_MASK;
+        }
     }
 
     // Adjustment applied, don't do it again.
