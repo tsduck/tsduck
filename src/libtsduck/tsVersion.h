1b6ff202fd1a35f7b4531bc4641b8dd6c9bf2f29
Thierry Lelegard <thierry@lelegard.fr>
2021-09-18 19:02:52 +0200

    Fixed tsconfig on macOS

--- src/libtsduck/tsVersion.h
+++ src/libtsduck/tsVersion.h
@@ -40,8 +40,8 @@
 //!
 //! TSDuck minor version.
 //!
-#define TS_VERSION_MINOR 28
+#define TS_VERSION_MINOR 29
 //!
 //! TSDuck commit number (automatically updated by Git hooks).
 //!
-#define TS_COMMIT 2551
+#define TS_COMMIT 2552
