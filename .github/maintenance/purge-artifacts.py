95f55f55c7e5e6a416313c94f64a0b4d3cf11bf4
Thierry Lelegard <thierry@lelegard.fr>
2022-09-22 23:19:42 +0200

    Fixed purge-artifacts.py script

--- .github/maintenance/purge-artifacts.py
+++ .github/maintenance/purge-artifacts.py
@@ -44,14 +44,21 @@ import tsgithub
 # The number of copies to keep per artifact.
 KEEP = 5
 
+print('Purging artifacts in repo %s, keeping %d copies of each artifact ...' % (tsgithub.repo_name, KEEP))
+
 # A dictionary of all artifact counts, by name.
 artifact_count = {}
+to_delete = []
 total_count = 0
 total_size = 0
 deleted_count = 0
 deleted_size = 0
+error_count = 0
+error_size = 0
 
-# Loop on all pages of artifats
+# Loop on all pages of artifacts.
+# We need to get all artifacts first, and then delete the extraneous ones.
+# If we read a page, delete artifacts, then read next page, the page boundaries have changed.
 artifacts_url = 'https://api.github.com/repos/' + tsgithub.repo_name + '/actions/artifacts'
 url = artifacts_url + '?per_page=100'
 while url is not None:
@@ -65,20 +72,33 @@ while url is not None:
             if 'name' in art and 'id' in art:
                 size = art['size'] if 'size' in art else 0
                 date = art['created_at'] if 'created_at' in art else ''
+                expired = 'expired' in art and art['expired']
                 total_count += 1
                 total_size += size
                 if art['name'] not in artifact_count:
+                    # First time we see this artifact.
                     artifact_count[art['name']] = 0
-                if ('expired' in art and art['expired']) or artifact_count[art['name']] >= KEEP:
-                    print('Deleting %s, id: %d, size: %d, date: %s' % (art['name'], art['id'], size, date))
-                    deleted_count += 1
-                    deleted_size += size
-                    requests.delete('%s/%d' % (artifacts_url, art['id']), auth = ('', tsgithub.token))
-                else:
+                if not expired and artifact_count[art['name']] < KEEP:
+                    # Keep this artifact.
                     artifact_count[art['name']] += 1
+                else:
+                    to_delete.append({'id': art['id'], 'name': art['name'], 'size': size, 'date': date})
+
+# Actually delete the artifacts.
+for art in to_delete:
+    print('Deleting %s, id: %d, size: %d, date: %s' % (art['name'], art['id'], art['size'], art['date']))
+    resp = requests.delete('%s/%d' % (artifacts_url, art['id']), auth = ('', tsgithub.token))
+    if resp.ok:
+        deleted_count += 1
+        deleted_size += size
+    else:
+        print('Error: status code: %d, reason: %s' % (resp.status_code, resp.reason))
+        error_count += 1
+        error_size += size
 
 # Print final summary.
 print('')
 print('== Total:     %5d files, %d bytes' % (total_count, total_size))
 print('== Deleted:   %5d files, %d bytes' % (deleted_count, deleted_size))
-print('== Remaining: %5d files, %d bytes' % (total_count - deleted_count, total_size - deleted_size))
+print('== Error:     %5d files, %d bytes' % (error_count, error_size))
+print('== Remaining: %5d files, %d bytes' % (total_count - deleted_count - error_count, total_size - deleted_size - error_size))
