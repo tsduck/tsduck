107ad4fbb54b60418020ad09f7dc4357799146d8
Thierry Lelegard <thierry@lelegard.fr>
2022-09-22 23:40:59 +0200

    Added script to list and purge 'close pending' issues

---
+++ .github/maintenance/close-pending.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+#
+#  TSDuck - The MPEG Transport Stream Toolkit
+#  Copyright (c) 2005-2022, Thierry Lelegard
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+#  THE POSSIBILITY OF SUCH DAMAGE.
+#
+#-----------------------------------------------------------------------------
+#
+#  List issues with "close pending" label in the TSDuck project on GitHub.
+#
+#  With option --purge, purge "close pending" issues:
+#  - Close open issues without update for 150 days.
+#  - Remove the "close pending" label to issues which were already closed.
+#
+#-----------------------------------------------------------------------------
+
+import sys
+import datetime
+import tsgithub
+
+max_age = 150 # days
+today = datetime.datetime.today().toordinal()
+purge = '--purge' in sys.argv
+
+# First, get all "close pending" issues at once.
+# We cannot do the cleanup in a get_issues() loop because we change the search criteria and the list can be paginated.
+pending_issues = [i for i in tsgithub.repo.get_issues(state = 'all', labels = ['close pending'])]
+
+# Then, do the cleanup in a second 
+for issue in pending_issues:
+    date = issue.updated_at.strftime('%Y-%m-%d')
+    age = today - issue.updated_at.toordinal()
+    comments = [] if issue.pull_request is None else ['PR']
+    comments.append(issue.state)
+    comments.extend([l.name for l in issue.labels if l.name != 'close pending'])
+    print('#%d: %s (%s), updated %s, %d days ago' % (issue.number, issue.title, ', '.join(comments), date, age))
+    if purge:
+        if issue.state == 'closed':
+            print('-> issue #%d is already closed, removing close pending label' % issue.number)
+            issue.remove_from_labels('close pending')
+        elif age > max_age and issue.pull_request is None:
+            print('-> issue #%d is too old, closing it' % issue.number)
+            issue.create_comment('Automatically closed after %d days without update and "close pending" label set.' % age)
+            issue.edit(state = 'closed')
+            issue.remove_from_labels('close pending')
