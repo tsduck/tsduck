38f479da9fd6fde91160368feba99490232057c6
Paul Higgs <paul_higgs@hotmail.com>
2022-10-11 15:06:42 +0100

    add TTML Subtitling Descriptor per ETSI EN 303 560

---
+++ src/libtsduck/dtv/descriptors/tsTTMLSubtitlingDescriptor.h
@@ -0,0 +1,87 @@
+//----------------------------------------------------------------------------
+//
+// TSDuck - The MPEG Transport Stream Toolkit
+// Copyright (c) 2022-, Paul Higgs
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+//----------------------------------------------------------------------------
+//!
+//!  @file
+//!  Representation of a TTML_subtitling_descriptor
+//!
+//----------------------------------------------------------------------------
+
+#pragma once
+#include "tsAbstractDescriptor.h"
+#include "tsVariable.h"
+
+namespace ts {
+    //!
+    //! Representation of a TTML_subtitling_descriptor.
+    //! @see ETSI EN 303 560, clause 5.2.1.1.
+    //! @ingroup descriptor
+    //!
+    class TSDUCKDLL TTMLSubtitlingDescriptor : public AbstractDescriptor
+    {
+    public:
+        // Public members:
+        UString              language_code;          //!< 24 bits, EN 303 560
+        uint8_t              subtitle_purpose;       //!< 6 bits, EN 303 560
+        uint8_t              TTS_suitability;        //<! 2 bits
+        std::vector<uint8_t> dvb_ttml_profile;       //!< set of 8 bit values
+        Variable<uint32_t>   qualifier;             //!< 32 bits, 
+        std::vector<uint8_t> font_id;                //!< set of 7 bit values
+        UString              service_name;
+        size_t               reserved_zero_future_use_bytes;  //<! arbitrary number of zero value bytes - not specified in EN 303 560
+
+        //!
+        //! Default constructor.
+        //!
+        TTMLSubtitlingDescriptor();
+
+        //!
+        //! Constructor from a binary descriptor
+        //! @param [in,out] duck TSDuck execution context.
+        //! @param [in] bin A binary descriptor to deserialize.
+        //!
+        TTMLSubtitlingDescriptor(DuckContext& duck, const Descriptor& bin);
+
+        // Inherited methods
+        DeclareDisplayDescriptor();
+
+    private:
+        static UString TTML_qualifier(uint32_t qualifier);
+        static UString TTML_subtitle_purpose(uint8_t purpose);
+        static UString TTML_suitability(uint8_t suitability);
+
+    protected:
+        // Inherited methods
+        virtual DID extendedTag() const override;
+        virtual void clearContent() override;
+        virtual void serializePayload(PSIBuffer&) const override;
+        virtual void deserializePayload(PSIBuffer&) override;
+        virtual void buildXML(DuckContext&, xml::Element*) const override;
+        virtual bool analyzeXML(DuckContext&, const xml::Element*) override;
+    };
+}
