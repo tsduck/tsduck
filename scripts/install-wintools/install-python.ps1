cf760c23c8aaab7106f99ead05e7cca9ccea148c
Thierry Lelegard <thierry@lelegard.fr>
2022-10-12 22:45:45 +0200

    Fixed Python install script when used in CI

--- scripts/install-wintools/install-python.ps1
+++ scripts/install-wintools/install-python.ps1
@@ -50,4 +50,6 @@ Install-Standard-Exe `
     "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" `
     @("/quiet", "InstallAllUsers=1", "CompileAll=1", "PrependPath=1", "Include_test=0")
 
+Propagate-Environment "Path"
+
 Exit-Script
