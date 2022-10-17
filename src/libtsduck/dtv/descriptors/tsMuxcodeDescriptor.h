a941f68b6bff1192b54b4949fb37977e7d0cda52
Paul Higgs <paul_higgs@hotmail.com>
2022-10-17 12:35:17 +0100

    add Muxcode descriptor according to ISO/IEC 13818-1 (8th Edition) and !4496-1 (4th Edition)

---
+++ src/libtsduck/dtv/descriptors/tsMuxcodeDescriptor.h
@@ -0,0 +1,100 @@
+//----------------------------------------------------------------------------
+//
+// TSDuck - The MPEG Transport Stream Toolkit
+// Copyright (c) 2022-, Paul Higgs
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+//----------------------------------------------------------------------------
+//!
+//!  @file
+//!  Representation of an MuxCode_descriptor
+//!
+//----------------------------------------------------------------------------
+
+#pragma once
+#include "tsAbstractDescriptor.h"
+#include "tsVariable.h"
+
+namespace ts {
+    //!
+    //! Representation of an MuxCode descriptor.
+    //!
+    //! @see ISO/IEC 13818-1, ITU-T Rec. H.222.0, 2.6.48 and ISO/IEC 14496-1, 7.4.2.5.
+    //! @ingroup descriptor
+    //!
+
+    class substructure_type {
+    public:
+        uint8_t     repititionCount;            //!< 3 bits
+        std::vector<uint8_t> m4MuxChannel;      //!< list of 8 bit values
+        std::vector<uint8_t> numberOfBytes;     //!< list of 8 bit values
+
+        //!
+        //! Default constructor.
+        //!
+        substructure_type();
+    };
+
+    class MuxCodeTableEntry_type {
+    public:
+        uint8_t     MuxCode;        //!< 4 bits
+        uint8_t     version;        //!< 4 bits
+        std::vector<substructure_type> substructure;
+
+        //!
+        //! Default constructor.
+        //!
+        MuxCodeTableEntry_type();
+    };
+
+    class TSDUCKDLL MuxCodeDescriptor : public AbstractDescriptor 
+    {
+    public:
+        // Public members:
+        std::vector<MuxCodeTableEntry_type> MuxCodeTableEntry;
+
+        //!
+        //! Default constructor.
+        //!
+        MuxCodeDescriptor();
+
+        //!
+        //! Constructor from a binary descriptor
+        //! @param [in,out] duck TSDuck execution context.
+        //! @param [in] bin A binary descriptor to deserialize.
+        //!
+        MuxCodeDescriptor(DuckContext& duck, const Descriptor& bin);
+
+        // Inherited methods
+        DeclareDisplayDescriptor();
+
+    protected:
+        // Inherited methods
+        virtual void clearContent() override;
+        virtual void serializePayload(PSIBuffer&) const override;
+        virtual void deserializePayload(PSIBuffer&) override;
+        virtual void buildXML(DuckContext&, xml::Element*) const override;
+        virtual bool analyzeXML(DuckContext&, const xml::Element*) override;
+    };
+}
