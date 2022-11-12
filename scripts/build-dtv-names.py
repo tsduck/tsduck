5b0c583428fa0d972f8efa25734e7efcacac3fdb
Thierry Lelegard <thierry@lelegard.fr>
2022-11-12 16:22:02 +0100

    Added Python script to generate DVT .names file

--- scripts/build-dtv-names.py
+++ scripts/build-dtv-names.py
@@ -28,26 +28,33 @@
 #
 #-----------------------------------------------------------------------------
 # 
-#  Build the file tsduck.dektec.names, containing Dektec-specific names.
-#  Syntax: build-dektec-names.py in-file out-file
+#  Build the file tsduck.dtv.names, containing names for Digital TV.
+#  Syntax: build-dtv-names-names.py out-file in-file-or-dir ... 
 # 
 #-----------------------------------------------------------------------------
 
-import tsbuild, sys, os, re
+import tsbuild, sys, os, fnmatch
 
-if len(sys.argv) != 3:
-    print('Usage: %s in-file out-file' % sys.argv[0], file=sys.stderr)
-    exit(1)
+# Process a .names file or a directory.
+def process_file_or_dir(path, output):
+    if os.path.isfile(path):
+        print('#---------- ' + os.path.basename(path), file=output)
+        print('', file=output)
+        with open(path, 'r', encoding='utf-8') as input:
+            output.write(input.read())
+        print('', file=output)
+    elif os.path.isdir(path):
+        for name in os.listdir(path):
+            subpath = path + os.sep + name
+            if os.path.isdir(subpath) or fnmatch.fnmatch(name, '*.names'):
+                process_file_or_dir(subpath, output)
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-    
-with open(output_file, 'w') as output:
-    tsbuild.write_source_header('#', 'Registered names for Dektec devices', file=output)
-    print('[DtCaps]', file=output)
-    if input_file != '':
-        with open(input_file, 'r', encoding='utf-8') as input:
-            for line in input:
-                match = re.search(r'#define\s+DTAPI_CAP_.*\sDtapi::DtCaps\(([\d]+)\)\s*//\s*(.+)$', line.strip())
-                if match is not None:
-                    print('%s = %s' % (match.group(1), match.group(2)), file=output)
+# Main code.
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: %s out-file in-file-or-dir ...' % sys.argv[0], file=sys.stderr)
+        exit(1)
+    with open(sys.argv[1], 'w', encoding='utf-8') as output:
+        tsbuild.write_source_header('#', 'Registered names for Digital TV', file=output)
+        for i in range(2, len(sys.argv)):
+            process_file_or_dir(sys.argv[i], output)
