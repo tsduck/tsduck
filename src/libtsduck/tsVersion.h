0348cb8e564669143b5534eb3f9d22b47b3e9e52
Thierry Lelegard <thierry@lelegard.fr>
2023-01-22 22:20:48 +0100

    Fixed link options

--- src/libtsduck/tsVersion.h
+++ src/libtsduck/tsVersion.h
@@ -40,8 +40,8 @@
 //!
 //! TSDuck minor version.
 //!
-#define TS_VERSION_MINOR 33
+#define TS_VERSION_MINOR 34
 //!
 //! TSDuck commit number (automatically updated by Git hooks).
 //!
-#define TS_COMMIT 3139
+#define TS_COMMIT 3140
