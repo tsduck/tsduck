7e7de2bf7da27af5aa83555a35f4efc5496778d0
Thierry Lelegard <thierry@lelegard.fr>
2023-04-09 12:59:13 +0200

    Added preconfiguration header for custom installations (issue #1201)

---
+++ src/libtsduck/base/cpp/tsPreConfiguration.h
@@ -0,0 +1,42 @@
+//----------------------------------------------------------------------------
+//
+//  TSDuck - The MPEG Transport Stream Toolkit
+//  Copyright (c) 2005-2023, Thierry Lelegard
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+//  THE POSSIBILITY OF SUCH DAMAGE.
+//
+//----------------------------------------------------------------------------
+//!
+//!  @file
+//!  @ingroup cpp
+//!  Pre-configuration header, defining configuration macros.
+//!
+//!  This header file shall remain empty in the source tree and in a full
+//!  standard installation. During installation in a special configuration,
+//!  with options excluding some dependencies (e.g. "make NOSRT=1 NOPCSC=1"),
+//!  the appropriate symbols are added in the copy of this file in the target
+//!  installation tree.
+//!
+//----------------------------------------------------------------------------
+
+#pragma once
