232a2e58e7dfacbb72a7b4f8bac5b748e59fe54d
Thierry Lelegard <thierry@lelegard.fr>
2024-03-20 22:19:35 +0000

    Added sample docker packaging

---
+++ pkg/docker/cleanup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#-----------------------------------------------------------------------------
+#
+#  TSDuck - The MPEG Transport Stream Toolkit
+#  Copyright (c) 2005-2024, Thierry Lelegard
+#  BSD-2-Clause license, see LICENSE.txt file or https://tsduck.io/license
+#
+#  Cleanup the image and all instances of the tsduck container.
+#
+#-----------------------------------------------------------------------------
+
+for cont in $(docker ps -a | awk '{print $1 " " $2}' | grep -e 'tsduck$' | awk '{print $1}'); do
+    docker container rm $cont
+done
+
+docker image rm tsduck
