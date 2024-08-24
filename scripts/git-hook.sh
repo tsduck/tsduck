5b956b560c0e3b39613382fb8ecf23ea9dfbd2e2
Thierry Lelegard <thierry@lelegard.fr>
2024-08-24 23:01:25 +0200

    Updated git hooks

--- scripts/git-hook.sh
+++ scripts/git-hook.sh
@@ -9,13 +9,17 @@
 #  It must be invoked with hook name as argument.
 #  Must be executable on Linux, MacOS and Windows (Git Bash).
 #
+#  Define environment variable GIT_HOOK_DEBUG to something non-empty
+#  to display additional information.
+#
 #-----------------------------------------------------------------------------
 
 HOOK="$1"
 SCRIPT=$(basename ${BASH_SOURCE[0]} .sh)
 ROOTDIR=$(cd $(dirname ${BASH_SOURCE[0]})/..; pwd)
-error() { echo >&2 "$SCRIPT: $*"; exit 1; }
-info()  { echo >&2 "$SCRIPT: $*"; }
+error() { echo >&2 "$SCRIPT: $HOOK: $*"; exit 1; }
+info()  { echo >&2 "$SCRIPT: $HOOK: $*"; }
+debug() { [[ -n "$GIT_HOOK_DEBUG" ]] && echo >&2 "$SCRIPT: (debug) $HOOK: $*"; }
 
 # Get current branch (output and syntax varies across versions of git).
 BRANCH=$(git branch | sed -e '/^$/d' -e '/^[^*]/d' -e 's/^\* *//' 2>/dev/null)
@@ -26,50 +30,60 @@ MASTER=master
 # Do nothing if branch is not "master".
 [[ "$BRANCH" == "$MASTER" ]] || exit 0
 
-# The commit number is in tsVersion.h
-SRCFILE="$ROOTDIR/src/libtsduck/tsVersion.h"
-PREFIX="#define TS_COMMIT"
-
 # Use GNU variants of sed and grep when available.
 # Add /usr/local/bin at end of PATH in case we can find them there.
 export PATH=$PATH:/usr/local/bin
 [[ -n "$(which gsed 2>/dev/null)" ]] && sed() { gsed "$@"; }
 [[ -n "$(which ggrep 2>/dev/null)" ]] && grep() { ggrep "$@"; }
 
+# The commit number is in tsVersion.h
+SRCFILE="$ROOTDIR/src/libtsduck/tsVersion.h"
+PREFIX="#define TS_COMMIT"
+
+# Max of two values.
+max() {
+    [[ "$1" -gt "$2" ]] && echo "$1" || echo "$2"
+}
+
+# Get commit from tsVersion.h.
+get-src-commit() {
+    [[ -e "$SRCFILE" ]] || error "$SRCFILE not found"
+    local commit=$(grep "^$PREFIX" "$SRCFILE" | head -1 | awk '{print $3}')
+    [[ -n "$commit" ]] || error "no commit count found in $SRCFILE"
+    debug "Commit from tsVersion.h: $commit"
+    echo $commit
+}
+
+# Update commit in tsVersion.h.
+set-src-commit() {
+    sed -i -e "/^$PREFIX/s/.*/$PREFIX $1/" "$SRCFILE"
+    git add "$SRCFILE"
+    info "Updated commit count to $1"
+}
+
+# Get number of commits on master branch.
+get-commit-count() {
+    local commit=$(git rev-list --count $MASTER)
+    debug "Number of commits on $MASTER: $commit"
+    echo $commit
+}
+
 # Filter by hook.
+debug "entering git hook"
 case "$HOOK" in
-
-    pre-commit|pre-merge-commit|post-merge)
-
-        # Get commit number in source file.
-        [[ -e "$SRCFILE" ]] || error "$SRCFILE not found"
-        SRCCOMMIT=$(grep "^$PREFIX" "$SRCFILE" | head -1 | awk '{print $3}')
-        [[ -n "$SRCCOMMIT" ]] || error "no commit count found in $SRCFILE"
-
-        # Compute the lowest next commit number to set.
-        # Can be forced externally using environment variable TS_GIT_COMMIT.
-        COMMIT="$TS_GIT_COMMIT"
-        if [[ -z "$COMMIT" ]]; then
-            # Get max number of commits from master branch. Previously, we
-            # did it on every local or remote branch but this could create
-            # inconsistencies on local repos with many branches.
-            COMMIT=$(git rev-list --count $MASTER)
-            [[ "$SRCCOMMIT" -gt "$COMMIT" ]] && COMMIT=$SRCCOMMIT
-            # With pre-commit, we must have at least this max + 1.
-            [[ "$HOOK" == "pre-commit" || "$HOOK" == "pre-merge-commit" ]] && COMMIT=$(($COMMIT + 1))
+    pre-commit)
+        if [[ -n "$TS_GIT_COMMIT" ]]; then
+            debug "Forced commit: $TS_GIT_COMMIT"
+            set-src-commit $TS_GIT_COMMIT
+        else
+            set-src-commit $(($(max $(get-commit-count) $(get-src-commit)) + 1))
         fi
-
-        # Update the commit count in source file if not up to date.
-        if [[ "$SRCCOMMIT" -lt "$COMMIT" ]]; then
-            sed -i -e "/^$PREFIX/s/.*/$PREFIX $COMMIT/" "$SRCFILE"
-            git add "$SRCFILE"
-            info "Updated commit count to $COMMIT"
-            # In post-merge, we commit the change. Increment number since we create a commit.
-            [[ "$HOOK" == "post-merge" ]] && TS_GIT_COMMIT=$(($COMMIT + 1)) git commit -m "Updated commit count to $COMMIT after merge"
-        fi
-        exit 0
+        ;;
+    post-merge)
+        export TS_GIT_COMMIT=$(($(max $(get-commit-count) $(get-src-commit)) + 1))
+        git commit -m "Updated commit count to $TS_GIT_COMMIT after merge"
         ;;
     *)
-        # Nothing to do in other hooks.
-        exit 0
+        ;;
 esac
+exit 0
