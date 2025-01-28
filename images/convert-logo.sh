2584006d412b80143c6d8e909e8e30e4069d690f
Thierry Lelegard <thierry@lelegard.fr>
2025-01-28 23:01:16 +0100

    Rebuilt PNG logos from SVG
    Removed previous XCF files (GIMP sources)

--- images/convert-logo.sh
+++ images/convert-logo.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 #
-# Convert a GIMP-generated .xcf file into many .png and .ico files of various sizes.
+# Convert the 512x512 PNG version of the logos to all sizes.
+#
+# Note: ImageMagick 7 is not able to convert from the SVG files: some paths are
+# not visible, the background is white instead of transparent, etc. We manually
+# convert the SVG files into the 512x512 PNG version using GIMP first. Then, we
+# convert this large PNG into all other sizes using ImageMagick.
 #
 
-for name in "$@"; do
-    infile="$name-512.xcf"
+convert-file() {
+    infile="$1"
+    outprefix="$2"
     if [[ -e "$infile" ]]; then
-        for size in 512 256 128 64 48 32 16; do
-            echo "generating $name-$size.png"
-            magick convert "$infile" -background transparent -flatten -resize ${size}x${size} "$name-$size.png"
+        for size in 256 128 64 48 32 16; do
+            echo "generating $outprefix-$size.png"
+            magick "$infile" -background none -flatten -resize ${size}x${size} "$outprefix-$size.png"
         done
-        echo "generating $name.ico"
-        magick convert "$infile" -background transparent -flatten -resize 128x128 "$name.ico"
+        echo "generating $outprefix.ico"
+        magick "$infile" -background none -flatten -resize 128x128 "$outprefix.ico"
     else
         echo >&2 "error: $infile not found"
     fi
-done
+}
+
+convert-file tsduck-512.png tsduck
+convert-file tsduck-extension-512.png tsduck-extension
