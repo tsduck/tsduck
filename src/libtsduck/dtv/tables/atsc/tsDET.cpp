d2ced40e2cd9c4d17f2bfe8d5334f09592b3d368
Thierry Lelegard <thierry@lelegard.fr>
2025-04-12 18:44:02 +0200

    Added DET from ATSC A/90

---
+++ src/libtsduck/dtv/tables/atsc/tsDET.cpp
@@ -0,0 +1,238 @@
+//----------------------------------------------------------------------------
+//
+// TSDuck - The MPEG Transport Stream Toolkit
+// Copyright (c) 2005-2025, Thierry Lelegard
+// BSD-2-Clause license, see LICENSE.txt file or https://tsduck.io/license
+//
+//----------------------------------------------------------------------------
+
+#include "tsDET.h"
+#include "tsBinaryTable.h"
+#include "tsTablesDisplay.h"
+#include "tsPSIRepository.h"
+#include "tsPSIBuffer.h"
+#include "tsDuckContext.h"
+#include "tsxmlElement.h"
+
+#define MY_XML_NAME u"DET"
+#define MY_CLASS ts::DET
+#define MY_TID ts::TID_DET
+#define MY_STD ts::Standards::ATSC
+
+TS_REGISTER_TABLE(MY_CLASS, {MY_TID}, MY_STD, MY_XML_NAME, MY_CLASS::DisplaySection);
+
+
+//----------------------------------------------------------------------------
+// Constructors
+//----------------------------------------------------------------------------
+
+ts::DET::DET(uint8_t version_, uint16_t source_id_) :
+    AbstractLongTable(MY_TID, MY_XML_NAME, MY_STD, version_, true), // DET is always "current"
+    source_id(source_id_),
+    data(this)
+{
+}
+
+ts::DET::DET(DuckContext& duck, const BinaryTable& table) :
+    DET()
+{
+    deserialize(duck, table);
+}
+
+
+ts::DET::DET(const DET& other) :
+    AbstractLongTable(other),
+    source_id(other.source_id),
+    protocol_version(other.protocol_version),
+    data(this, other.data)
+{
+}
+
+ts::DET::Data::Data(const AbstractTable* table) :
+    EntryWithDescriptors(table)
+{
+}
+
+
+//----------------------------------------------------------------------------
+// Inherited public methods
+//----------------------------------------------------------------------------
+
+uint16_t ts::DET::tableIdExtension() const
+{
+    return source_id;
+}
+
+
+//----------------------------------------------------------------------------
+// Clear the content of the table.
+//----------------------------------------------------------------------------
+
+void ts::DET::clearContent()
+{
+    source_id = 0;
+    protocol_version = 0;
+    data.clear();
+}
+
+
+//----------------------------------------------------------------------------
+// Deserialization
+//----------------------------------------------------------------------------
+
+void ts::DET::deserializePayload(PSIBuffer& buf, const Section& section)
+{
+    // Get common properties (should be identical in all sections)
+    source_id = section.tableIdExtension();
+    protocol_version = buf.getUInt8();
+    size_t num_data_in_section = buf.getUInt8();
+
+    // Get events description
+    while (!buf.error() && num_data_in_section-- > 0) {
+        Data& event(data.newEntry());
+        buf.skipReservedBits(2);
+        buf.getBits(event.data_id, 14);
+        event.start_time = Time::GPSSecondsToUTC(cn::seconds(buf.getUInt32()));
+        buf.skipReservedBits(2);
+        buf.getBits(event.ETM_location, 2);
+        buf.getBits(event.length_in_seconds, 20);
+        buf.getMultipleStringWithLength(event.title_text);
+        buf.getDescriptorListWithLength(event.descs);
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// Serialization
+//----------------------------------------------------------------------------
+
+void ts::DET::serializePayload(BinaryTable& table, PSIBuffer& buf) const
+{
+    // Add fixed fields.
+    buf.putUInt8(protocol_version);
+
+    // Save position before num_data_in_section. Will be updated at each event.
+    uint8_t num_data_in_section = 0;
+    buf.pushState();
+    buf.putUInt8(num_data_in_section);
+    const size_t payload_min_size = buf.currentWriteByteOffset();
+
+    // Loop on event definitions.
+    for (auto it = data.begin(); !buf.error() && it != data.end(); ++it) {
+        const Data& event(it->second);
+
+        // Pre-serialize the title_text. Its max size is 255 bytes since its size must fit in a byte.
+        ByteBlock title;
+        event.title_text.serialize(buf.duck(), title, 255, true);
+
+        // Binary size of the event definition.
+        const size_t entry_size = 10 + title.size() + 2 + event.descs.binarySize();
+
+        // If we are not at the beginning of the event loop, make sure that the entire
+        // event fits in the section. If it does not fit, start a new section.
+        if (entry_size > buf.remainingWriteBytes() && buf.currentWriteByteOffset() > payload_min_size) {
+            // Create a new section.
+            addOneSection(table, buf);
+            // We are at the position of num_data_in_section in the new section.
+            buf.putUInt8(num_data_in_section = 0);
+        }
+
+        // Serialize the event definition.
+        buf.putReserved(2);
+        buf.putBits(event.data_id, 14);
+        buf.putUInt32(uint32_t(event.start_time.toGPSSeconds().count()));
+        buf.putReserved(2);
+        buf.putBits(event.ETM_location, 2);
+        buf.putBits(event.length_in_seconds, 20);
+        buf.putUInt8(uint8_t(title.size()));
+        buf.putBytes(title);
+        buf.putPartialDescriptorListWithLength(event.descs);
+
+        // Now increment the field num_data_in_section at saved position.
+        buf.swapState();
+        buf.pushState();
+        buf.putUInt8(++num_data_in_section);
+        buf.popState();
+        buf.swapState();
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// A static method to display a DET section.
+//----------------------------------------------------------------------------
+
+void ts::DET::DisplaySection(TablesDisplay& disp, const ts::Section& section, PSIBuffer& buf, const UString& margin)
+{
+    disp << margin << UString::Format(u"Source Id: %n", section.tableIdExtension()) << std::endl;
+
+    size_t event_count = 0;
+
+    if (buf.canReadBytes(2)) {
+        disp << margin << UString::Format(u"Protocol version: %d", buf.getUInt8());
+        disp << UString::Format(u", number of data events: %d", event_count = buf.getUInt8()) << std::endl;
+
+        // Loop on all event definitions.
+        DescriptorContext context(disp.duck(), section.tableId(), section.definingStandards(disp.duck().standards()));
+        while (buf.canReadBytes(8) && event_count-- > 0) {
+            buf.skipReservedBits(2);
+            disp << margin << UString::Format(u"- Data Id: %n", buf.getBits<uint16_t>(14)) << std::endl;
+            disp << margin << "  Start UTC: " << Time::GPSSecondsToUTC(cn::seconds(buf.getUInt32())).format(Time::DATETIME) << std::endl;
+            buf.skipReservedBits(2);
+            disp << margin << UString::Format(u"  ETM location: %d", buf.getBits<uint8_t>(2)) << std::endl;
+            disp << margin << UString::Format(u"  Duration: %d seconds", buf.getBits<uint32_t>(20)) << std::endl;
+            disp.displayATSCMultipleString(buf, 1, margin + u"  ", u"Title text: ");
+            disp.displayDescriptorListWithLength(section, context, false, buf, margin + u"  ");
+        }
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// XML serialization
+//----------------------------------------------------------------------------
+
+void ts::DET::buildXML(DuckContext& duck, xml::Element* root) const
+{
+    root->setIntAttribute(u"version", _version);
+    root->setIntAttribute(u"source_id", source_id, true);
+    root->setIntAttribute(u"protocol_version", protocol_version);
+
+    for (const auto& it : data) {
+        xml::Element* e = root->addElement(u"data");
+        e->setIntAttribute(u"data_id", it.second.data_id, true);
+        e->setDateTimeAttribute(u"start_time", it.second.start_time);
+        e->setIntAttribute(u"ETM_location", it.second.ETM_location, true);
+        e->setChronoAttribute(u"length_in_seconds", it.second.length_in_seconds, false);
+        it.second.title_text.toXML(duck, e, u"title_text", true);
+        it.second.descs.toXML(duck, e);
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// XML deserialization
+//----------------------------------------------------------------------------
+
+bool ts::DET::analyzeXML(DuckContext& duck, const xml::Element* element)
+{
+    xml::ElementVector xdata;
+    bool ok =
+        element->getIntAttribute(_version, u"version", false, 0, 0, 31) &&
+        element->getIntAttribute(source_id, u"source_id", true) &&
+        element->getIntAttribute(protocol_version, u"protocol_version", false, 0) &&
+        element->getChildren(xdata, u"data");
+
+    // Get all data events.
+    for (size_t i = 0; ok && i < xdata.size(); ++i) {
+        Data& event(data.newEntry());
+        xml::ElementVector xtitle;
+        ok = xdata[i]->getIntAttribute(event.data_id, u"data_id", true, 0, 0, 0x3FFF) &&
+             xdata[i]->getDateTimeAttribute(event.start_time, u"start_time", true) &&
+             xdata[i]->getIntAttribute(event.ETM_location, u"ETM_location", true, 0, 0, 3) &&
+             xdata[i]->getChronoAttribute(event.length_in_seconds, u"length_in_seconds", true, cn::seconds::zero(), cn::seconds::zero(), cn::seconds(0x000FFFFF)) &&
+             event.descs.fromXML(duck, xtitle, xdata[i], u"title_text") &&
+             (xtitle.empty() || event.title_text.fromXML(duck, xtitle[0]));
+    }
+    return ok;
+}
--- src/libtsduck/dtv/tables/atsc/tsDET.h
+++ src/libtsduck/dtv/tables/atsc/tsDET.h
@@ -7,7 +7,7 @@
 //----------------------------------------------------------------------------
 //!
 //!  @file
-//!  Representation of an ATSC Event Information Table (EIT)
+//!  Representation of an ATSC Data Event Table (DET)
 //!
 //----------------------------------------------------------------------------
 
@@ -19,72 +19,72 @@
 
 namespace ts {
     //!
-    //! Representation of an ATSC Event Information Table (ATSC EIT).
-    //! @see ATSC A/65, section 6.5.
+    //! Representation of an ATSC Data Event Table (DET).
+    //! @see ATSC A/95, section 11.3.1.
     //! @ingroup libtsduck table
     //!
-    class TSDUCKDLL ATSCEIT : public AbstractLongTable
+    class TSDUCKDLL DET : public AbstractLongTable
     {
     public:
         //!
-        //! Description of an event.
+        //! Description of a data event.
         //! Note: by inheriting from EntryWithDescriptors, there is a public field "DescriptorList descs".
         //!
-        class TSDUCKDLL Event : public EntryWithDescriptors
+        class TSDUCKDLL Data : public EntryWithDescriptors
         {
-            TS_NO_DEFAULT_CONSTRUCTORS(Event);
-            TS_DEFAULT_ASSIGMENTS(Event);
+            TS_NO_DEFAULT_CONSTRUCTORS(Data);
+            TS_DEFAULT_ASSIGMENTS(Data);
         public:
             // Public members
-            uint16_t           event_id = 0;          //!< Event id, 14 bits.
-            Time               start_time {};         //!< Event start_time.
+            uint16_t           data_id = 0;           //!< Data id, 14 bits.
+            Time               start_time {};         //!< Data start_time.
             uint8_t            ETM_location = 0;      //!< Location of extended text message, 2 bits.
-            cn::seconds        length_in_seconds {};  //!< Event duration in seconds, 20 bits.
+            cn::seconds        length_in_seconds {};  //!< Data duration in seconds, 20 bits.
             ATSCMultipleString title_text {};         //!< Multi-lingual event title.
 
             //!
             //! Constructor.
             //! @param [in] table Parent EIT.
             //!
-            Event(const AbstractTable* table);
+            Data(const AbstractTable* table);
         };
 
         //!
-        //! List of events.
+        //! List of data events.
         //!
-        using EventList = EntryWithDescriptorsList<Event>;
+        using DataList = EntryWithDescriptorsList<Data>;
 
-        // ATSCEIT public members:
-        uint16_t  source_id = 0;         //!< EIT source id.
-        uint8_t   protocol_version = 0;  //!< ATSC protocol version.
-        EventList events;                //!< List of events.
+        // DET public members:
+        uint16_t source_id = 0;         //!< Data source id.
+        uint8_t  protocol_version = 0;  //!< ATSC protocol version.
+        DataList data;                  //!< List of data events.
 
         //!
         //! Default constructor.
         //! @param [in] version Table version number.
-        //! @param [in] source_id Event source id.
+        //! @param [in] source_id Data source id.
         //!
-        ATSCEIT(uint8_t version = 0, uint16_t source_id = 0);
+        DET(uint8_t version = 0, uint16_t source_id = 0);
 
         //!
         //! Constructor from a binary table.
         //! @param [in,out] duck TSDuck execution context.
         //! @param [in] table Binary table to deserialize.
         //!
-        ATSCEIT(DuckContext& duck, const BinaryTable& table);
+        DET(DuckContext& duck, const BinaryTable& table);
 
         //!
         //! Copy constructor.
         //! @param [in] other Other instance to copy.
         //!
-        ATSCEIT(const ATSCEIT& other);
+        DET(const DET& other);
 
         //!
         //! Assignment operator.
         //! @param [in] other Other instance to copy.
         //! @return A reference to this object.
         //!
-        ATSCEIT& operator=(const ATSCEIT& other) = default;
+        DET& operator=(const DET& other) = default;
 
         // Inherited methods
         virtual uint16_t tableIdExtension() const override;
