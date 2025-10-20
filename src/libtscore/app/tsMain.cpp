a8cc98e1b6a5f52ae901707c6efef5b39d84e59f
Thierry Lelegard <thierry@lelegard.fr>
2025-10-20 22:21:33 +0200

    Fixed non-ASCII characters on Windows command line

--- src/libtscore/app/tsMain.cpp
+++ src/libtscore/app/tsMain.cpp
@@ -11,28 +11,45 @@
 #include "tsIPUtils.h"
 #include "tsCOM.h"
 
+#if defined(TS_WINDOWS)
+#include "tsUString.h"
+#endif
+
 
 //----------------------------------------------------------------------------
 // A function to wrap the entry point of an application.
 //----------------------------------------------------------------------------
 
-int MainWrapper(int (*func)(int argc, char* argv[]), int argc, char* argv[])
+int MainWrapper(int (*func)(int argc, char* argv[]), int argc, _TS_ARGV_TYPE* argv[])
 {
     // Save console state, set UTF-8 output, restore state on exit.
     ts::ConsoleState _consoleState;
 
     try {
-
 #if defined(TS_WINDOWS)
         // Initialize COM and networking.
         ts::COM com;
         if (!com.isInitialized() || !ts::IPInitialize()) {
             return EXIT_FAILURE;
         }
-#endif
+
+        // Convert UTF-16 command line arguments to UTF-8 and rebuild a 8-bit version of argv.
+        std::vector<std::string> arg8(argc);
+        std::vector<char*> argv8(argc);
+        for (int i = 0; i < argc; i++) {
+            ts::UString(argv[i]).toUTF8(arg8[i]);
+            argv8[i] = arg8[i].data();
+        }
+
+        // Argv is traditionally followed by a null pointer.
+        argv8.push_back(nullptr);
 
         // Actual application code.
+        return func(argc, argv8.data());
+#else
+        // Actual application code.
         return func(argc, argv);
+#endif
     }
     catch (const std::exception& e) {
         std::cerr << "Program aborted: " << e.what() << std::endl;
--- src/libtscore/app/tsMain.h
+++ src/libtscore/app/tsMain.h
@@ -15,22 +15,47 @@
 #include "tsArgs.h"
 #include "tsLibTSCoreVersion.h"
 
+//! @cond nodoxygen
+// See comments below.
+#if defined(TS_WINDOWS)
+    #define _TS_MAIN_NAME wmain
+    #define _TS_ARGV_TYPE wchar_t
+#else
+    #define _TS_MAIN_NAME main
+    #define _TS_ARGV_TYPE char
+#endif
+//! @endcond
+
 //!
 //! A function to wrap the entry point of an application.
-//! The application code should use the macro TS_MAIN instead of
-//! directly calling this function.
+//! @ingroup app
+//!
+//! The application code should use the macro TS_MAIN instead of directly calling this function.
 //!
 //! Uncaught exceptions are displayed.
-//! On Windows, the COM environment and IP networking are initialized.
+//!
+//! Windows specificities
+//! ---------------------
+//! The COM environment and IP networking are initialized.
+//!
 //! The Windows console is set to UTF-8 mode and restored to its previous value on exit.
 //!
-//! @ingroup app
+//! The entry point is defined as wmain() instead of main() in macro TS_MAIN to capture
+//! UTF-16 characters from the command line. This is a workaround for another Windows oddity.
+//! On Windows, unlike any modern operating system, when main() is used, any non-ASCII
+//! character from the command line is converted to '?' instead of converting all strings
+//! to UTF-8. We need to use wmain() to get UTF-16 characters. In MainWrapper(), we convert
+//! UTF-16 characters from the command line into UTF-8 to be compatible with the classical
+//! main() UTF-8 profile. In that case, there is a double conversion, first from UTF-16 to
+//! UTF-8 in MainWrapper(), then back from UTF-8 to UTF-16 inside the application, typically
+//! in class Args. This is silly but an operating not working in UTF-8 is silly...
+//!
 //! @param [in] func The actual main function with the same profile as main().
 //! @param [in] argc Command line parameter count.
 //! @param [in] argv Command line parameters.
 //! @return The process exit code, typically EXIT_SUCCESS or EXIT_FAILURE.
 //!
-int TSCOREDLL MainWrapper(int (*func)(int argc, char* argv[]), int argc, char* argv[]);
+int TSCOREDLL MainWrapper(int (*func)(int argc, char* argv[]), int argc, _TS_ARGV_TYPE* argv[]);
 
 //!
 //! A macro which expands to a main() program.
@@ -41,8 +66,8 @@ int TSCOREDLL MainWrapper(int (*func)(int argc, char* argv[]), int argc, char* a
 //! @hideinitializer
 //!
 #define TS_MAIN(func)                                  \
-    static int func(int argc, char *argv[]);           \
-    int main(int argc, char *argv[])                   \
+    static int func(int argc, char* argv[]);           \
+    int _TS_MAIN_NAME(int argc, _TS_ARGV_TYPE* argv[]) \
     {                                                  \
         TS_LIBTSCORE_CHECK();                          \
         return MainWrapper(func, argc, argv);          \
