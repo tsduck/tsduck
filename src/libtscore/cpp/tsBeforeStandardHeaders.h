c9ac53aca47196a047abc9da0b09d63466d771b8
Thierry Lelegard <thierry@lelegard.fr>
2025-11-13 22:18:58 +0100

    Update for Visual Studio 2026

--- src/libtscore/cpp/tsBeforeStandardHeaders.h
+++ src/libtscore/cpp/tsBeforeStandardHeaders.h
@@ -69,7 +69,7 @@
 // The normal warning reporting is restored after inclusion.
 // See the restoration section in tsAfterStandardHeaders.h.
 TS_PUSH_WARNING()
-TS_MSC_NOWARNING(4193 4244 4263 4264 4668 4774 5026 5027 5031 5032 5054 5204 5262 5266)
+TS_MSC_NOWARNING(4193 4244 4263 4264 4668 4774 4865 5026 5027 5031 5032 5054 5204 5262 5266)
 TS_GCC_NOWARNING(pedantic)
 TS_GCC_NOWARNING(cast-function-type-strict)  // OpenSSL 3.3
 TS_LLVM_NOWARNING(reserved-id-macro)
