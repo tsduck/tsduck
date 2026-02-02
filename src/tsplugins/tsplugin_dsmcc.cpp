a204d5d0bae1908553da0733b08fd88e73bc5772
Piotr Serafin <piotr.serafin.71@gmail.com>
2026-02-03 00:35:38 +0100

    feat: formatting and options (list, output)

--- src/tsplugins/tsplugin_dsmcc.cpp
+++ src/tsplugins/tsplugin_dsmcc.cpp
@@ -24,30 +24,34 @@
 //----------------------------------------------------------------------------
 
 namespace ts {
-    class DSMCCPlugin: public ProcessorPlugin, private TableHandlerInterface {
+    class DSMCCPlugin: public ProcessorPlugin, private TableHandlerInterface
+    {
         TS_PLUGIN_CONSTRUCTORS(DSMCCPlugin);
 
     public:
         // Implementation of plugin API
-        virtual bool   start() override;
-        virtual bool   getOptions() override;
+        virtual bool start() override;
+        virtual bool getOptions() override;
         virtual Status processPacket(TSPacket&, TSPacketMetadata&) override;
 
     private:
         using ModulesMap = std::map<uint16_t, ByteBlock>;
 
-        bool         _abort = false;               // Error (not found, etc).
-        PID          _pid = PID_NULL;              // Carousel PID.
-        bool         _dsi_found = false;           // DSI found flag
-        bool         _dii_found = false;           // DII found flag
-        bool         _all_modules_found = false;   // All modules found flag
-        uint16_t     _number_of_modules = 0x0000;  // Number of modules
-        ModulesMap   _modules {};                  // Map of modules data
-        SectionDemux _demux {duck, this};          // Section filter
+        bool _abort = false;                   // Error (not found, etc).
+        PID _pid = PID_NULL;                   // Carousel PID.
+        bool _dsi_found = false;               // DSI found flag
+        bool _dii_found = false;               // DII found flag
+        bool _all_modules_found = false;       // All modules found flag
+        uint16_t _number_of_modules = 0x0000;  // Number of modules
+        ModulesMap _modules {};                // Map of modules data
+        SectionDemux _demux {duck, this};      // Section filter
 
         bool _dump_ready = false;
 
         std::ostream* _out = &std::cout;
+
+        bool _opt_list = false;
+        UString _opt_out_dir {};
 
         // Invoked by the demux when a complete table is available.
         virtual void handleTable(SectionDemux&, const BinaryTable&) override;
@@ -69,8 +73,13 @@ ts::DSMCCPlugin::DSMCCPlugin(TSP* tsp_) :
     ProcessorPlugin(tsp_, u"Extract DSM-CC content", u"[options]")
 {
     option(u"pid", 'p', PIDVAL);
-    help(u"pid",
-         u"Specifies the PID carrying DSM-CC Object Carousel.");
+    help(u"pid", u"Specifies the PID carrying DSM-CC Object Carousel. This is a required parameter.");
+
+    option(u"list", 'l');
+    help(u"list", u"List the content of the object carousel instead of extracting files.");
+
+    option(u"output-directory", 'o', STRING);
+    help(u"output-directory", u"Specify the directory where carousel files will be extracted. Required unless --list is used.");
 }
 
 //----------------------------------------------------------------------------
@@ -81,6 +90,19 @@ bool ts::DSMCCPlugin::getOptions()
 {
     getIntValue(_pid, u"pid");
     verbose(u"get options pid: %n", _pid);
+
+    _opt_list = present(u"list");
+    getValue(_opt_out_dir, u"output-directory");
+
+    if (_pid == PID_NULL) {
+        error(u"a PID must be specified using --pid");
+        return false;
+    }
+
+    if (!_opt_list && _opt_out_dir.empty()) {
+        error(u"an output directory must be specified with --output-directory unless --list is used");
+        return false;
+    }
     return true;
 }
 //----------------------------------------------------------------------------
@@ -167,7 +189,7 @@ void ts::DSMCCPlugin::processUNM(const DSMCCUserToNetworkMessage& unm)
         _dii_found = true;
         verbose(u"DII found");
         verbose(u"number of modules: %n", unm.modules.size());
-        _number_of_modules = unm.modules.size();
+        _number_of_modules = static_cast<uint16_t>(unm.modules.size());
     }
 }
 
@@ -185,8 +207,8 @@ void ts::DSMCCPlugin::processDDM(const DSMCCDownloadDataMessage& ddm)
 
     if (_dsi_found && _dii_found) {
         verbose(u"dsi and dii found!!!");
-        verbose(u"modules captured: %d", (uint16_t)_number_of_modules);
-        if (_number_of_modules == (uint16_t)_modules.size()) {
+        verbose(u"modules captured: %d", static_cast<uint16_t>(_number_of_modules));
+        if (_number_of_modules == static_cast<uint16_t>(_modules.size())) {
             _all_modules_found = true;
             verbose(u"ALL MODULES FOUND!");
             for (auto& module : _modules) {
