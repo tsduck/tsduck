82591279146cf17301d9deb6ead233a91001a919
Thierry Lelegard <thierry@lelegard.fr>
2026-05-25 00:01:14 +0200

    Updated nightly build schedule

--- pkg/github/nightly-build-statistics.py
+++ pkg/github/nightly-build-statistics.py
@@ -15,16 +15,20 @@ import sys, tsgithub
 repo = tsgithub.repository(sys.argv)
 repo.check_opt_final()
 
-# Get all runs of workflow nightly-build.yml.
 workflow = repo.repo.get_workflow('nightly-build.yml')
+
+print('  Run#   Created               Started               Duration   Status     Origin')
+print('------   -------------------   -------------------   --------   --------   ---------')
+
 for run in workflow.get_runs():
+    created = run.created_at.strftime('%Y-%m-%d %H:%M:%S')
     started = run.run_started_at.strftime('%Y-%m-%d %H:%M:%S')
     try:
         seconds = int(run.timing().run_duration_ms / 1000)
     except:
         seconds = 0
     duration = '%02d:%02d:%02d' % (seconds // 3600, (seconds // 60) % 60, seconds % 60)
-    print('%6d   %s   %s   %s   %s' % (run.run_number, started, duration, run.conclusion, run.event))
+    print('%6d   %s   %s   %s   %-8s   %s' % (run.run_number, created, started, duration, run.conclusion, run.event))
 
 
 
